1963455dbcb39c0bd1d29baddcbde4efd5e015af
pHH <gudghks56@gmail.com>
2023-06-24 23:31:31 +0900

    feat: 마이페이지 뷰 추가

--- CodeStack/Presentation/MyPage/MyPageViewController.swift
+++ CodeStack/Presentation/MyPage/MyPageViewController.swift
@@ -6,16 +6,30 @@
 //
 
 import UIKit
+import SnapKit
 
 class MyPageViewController: UIViewController{
     
-    
+    let profileView: ProfileView = {
+        let view = ProfileView(frame: .zero)
+        view.layer.cornerRadius = 12
+        return view
+    }()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        configure()
+        self.view.backgroundColor = UIColor.systemBackground
+    }
+    
+    private func configure(){
+        view.addSubview(profileView)
+        profileView.snp.makeConstraints{
+            $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(-44)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(250).priority(.low)
+        }
     }
     
     
--- CodeStack/Presentation/MyPage/ProfileView.swift
+++ CodeStack/Presentation/MyPage/ProfileView.swift
@@ -11,16 +11,61 @@ import SnapKit
 class ProfileView: UIView{
     
     
-    private lazy var imageView: UIImageView = {
+    private let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(systemName: "person")
-        imageView.tintColor = UIColor.yellow
+        imageView.tintColor = UIColor.gray
+        imageView.layer.cornerRadius = 12
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.layer.borderWidth = 0.1
         return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+       let label = UILabel()
+        label.text = "UserName"
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.numberOfLines = 2
+        label.textAlignment = .left
+        label.minimumScaleFactor = 0.7
+        return label
+    }()
+    
+    
+    private let rank: UILabel = {
+        let label = UILabel()
+        label.text = "Rank"
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.numberOfLines = 1
+        label.textColor = .lightGray
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private let rankLabel: UILabel = {
+        let label = UILabel()
+         label.text = "3,412,232"
+         label.font = UIFont.boldSystemFont(ofSize: 16)
+         label.numberOfLines = 1
+        label.textAlignment = .left
+         label.minimumScaleFactor = 0.7
+         return label
+     }()
+    
+    private let editButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Edit profile", for: .normal)
+        button.setTitleColor(.systemGreen, for: .normal)
+        button.backgroundColor = UIColor.systemGreen.withAlphaComponent(0.3)
+        button.layer.cornerRadius = 12
+        return button
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        self.backgroundColor = .tertiarySystemBackground
+        addAutoLayout()
     }
     
     required init?(coder: NSCoder) {
@@ -29,15 +74,43 @@ class ProfileView: UIView{
     }
     
     func addAutoLayout(){
-        self.addSubview(imageView)
+        
+        [imageView,nameLabel,rank,rankLabel,editButton].forEach{
+            self.addSubview($0)
+        }
         
         imageView.snp.makeConstraints{
-            $0.width.height.equalTo(50)
-            $0.top.equalToSuperview().inset(40)
+            $0.width.height.equalTo(80)
+            $0.top.leading.equalToSuperview().inset(16)
+            $0.leading.equalToSuperview().inset(12)
+        }
+        nameLabel.snp.makeConstraints{
+            $0.top.equalTo(imageView.snp.top).offset(12)
+            $0.leading.equalTo(imageView.snp.trailing).offset(12)
+            $0.trailing.equalToSuperview().offset(-12)
+        }
+        
+        
+        rank.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom).offset(-25)
+            $0.leading.equalTo(nameLabel.snp.leading)
+        }
+        rank.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+        rank.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        
+        rankLabel.snp.makeConstraints{
+            $0.top.equalTo(imageView.snp.bottom).offset(-25)
+            $0.leading.equalTo(rank.snp.trailing).offset(8)
+            $0.trailing.equalTo(nameLabel.snp.trailing)
+        }
+        
+        editButton.snp.makeConstraints{
+            $0.top.equalTo(imageView.snp.bottom).offset(12)
+            $0.leading.trailing.equalToSuperview().inset(12)
+            $0.height.equalTo(44)
+            $0.bottom.equalToSuperview().inset(16).priority(.high)
         }
     }
-    
-    
     
     
 }
