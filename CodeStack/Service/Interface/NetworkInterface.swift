f2bd6da8196e1fda9528c8fe2adec27baacc0e9f
pHH <gudghks56@gmail.com>
2023-07-03 23:03:20 +0900

    move: 프로토콜 코드 분리

---
+++ CodeStack/Service/Interface/NetworkInterface.swift
@@ -0,0 +1,43 @@
+//
+//  NetworkInterface.swift
+//  CodeStack
+//
+//  Created by 박형환 on 2023/07/03.
+//
+
+import Foundation
+import RxSwift
+
+typealias OAuthrizationRequest = GitOAuthrizationRequest & OAuthrization
+
+protocol AppleAuth: AnyObject{
+    func request(with token: AppleToken) -> Maybe<String>
+}
+
+protocol GitOAuthrizationRequest: AnyObject{
+    func gitOAuthrization() throws
+    func request(code: String) -> Maybe<GitToken>
+    func request(with token: GitToken, provider: OAuthProvider) -> Maybe<Void>
+    func endPoint(url string: String) -> URL
+}
+
+protocol OAuthrization{
+    func getBaseURL(provider: OAuthProvider) -> String
+}
+
+
+protocol ApolloService{
+    func request() -> SubmissionPagedResult
+}
+
+
+//MARK: TestMock 삭제예졍
+
+class NetworkService: ApolloService{
+    
+    func request() -> SubmissionPagedResult {
+        let sub = Submission.dummy()
+        let result = SubmissionPagedResult(content: sub, pageInfo: PageInfo(totalElement: 1, totalPage: 1))
+        return result
+    }
+}
