feed77ec410f3b5ac2e1aaac828ed9b4c48674ee
pHH <gudghks56@gmail.com>
2023-07-14 23:04:14 +0900

    feat: OnBoarding 플로우 추가

---
+++ CodeStack/Flow/OnBoardingFlow.swift
@@ -0,0 +1,51 @@
+//
+//  OnBoardingFlow.swift
+//  CodeStack
+//
+//  Created by 박형환 on 2023/07/14.
+//
+
+import UIKit
+import RxFlow
+
+class OnBoardingFlow: Flow{
+    
+    var root: Presentable{
+        rootViewController
+    }
+    
+    var rootViewController: UINavigationController = {
+        let viewController = UINavigationController()
+        viewController.navigationBar.topItem?.title = "OnBoarding"
+        return viewController
+    }()
+    
+    
+    func navigate(to step: Step) -> FlowContributors {
+        guard
+            let codeStep = step as? CodestackStep
+        else { Log.error("Step unwrappingError");  return .none}
+        switch codeStep{
+        case .onBoardingRequired:
+            return navigateToOnBoardingVC()
+        case .onBoardingComplte:
+            return .end(forwardToParentFlowWithStep: CodestackStep.onBoardingComplte)
+        default:
+            return .none
+        }
+    }
+    
+    private func navigationToLoginScreen() -> FlowContributors {
+        return .none
+    }
+    
+    func navigateToOnBoardingVC() -> FlowContributors{
+        let onBoarding = OnBoardingVC()
+        
+        Log.debug(rootViewController.isNavigationBarHidden)
+        
+        rootViewController.pushViewController(onBoarding, animated: false)
+        
+        return .one(flowContributor: .contribute(withNext: onBoarding))
+    }
+}
