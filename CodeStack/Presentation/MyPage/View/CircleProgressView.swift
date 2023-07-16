fe9098017094e8eb324009e2af11794f19a967d3
pHH <gudghks56@gmail.com>
2023-07-16 23:10:46 +0900

    refactor: 이름 변경

---
+++ CodeStack/Presentation/MyPage/View/CircleProgressView.swift
@@ -0,0 +1,84 @@
+//
+//  CircleProgressView.swift
+//  CodeStack
+//
+//  Created by 박형환 on 2023/06/25.
+//
+
+import UIKit
+class CircleProgressView: UIView {
+    
+    
+    public var progressColor: UIColor? {
+        didSet {
+            circleLayer.progressColor = progressColor
+            circleLayer.setNeedsDisplay()
+        }
+    }
+    
+    public var progressBackgroundColor: UIColor? {
+        didSet {
+            circleLayer.progressBackgroundColor = progressBackgroundColor
+            circleLayer.setNeedsDisplay()
+        }
+    }
+    
+    public var progress: CGFloat = 0 {
+        didSet {
+            circleLayer.progress = progress
+            circleLayer.setNeedsDisplay()
+        }
+    }
+    
+    public var progressWidth: CGFloat = 0 {
+        didSet {
+            circleLayer.progressWidth = progressWidth
+            circleLayer.setNeedsDisplay()
+        }
+    }
+    
+    public var clockwise: Bool {
+        set {
+            circleLayer.clockwise = !newValue
+            circleLayer.setNeedsDisplay()
+        }
+        get {
+            return !circleLayer.clockwise
+        }
+    }
+    
+    private var circleLayer: CircleLayer {
+        return layer as! CircleLayer
+    }
+    
+    public override class var layerClass: AnyClass {
+        return CircleLayer.self
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .tertiarySystemBackground
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    
+    
+    public func configureAttributes(color: UIColor,
+                                    background: UIColor,
+                                    progress: CGFloat,
+                                    clockwise: Bool = false){
+        
+        self.progressColor = color
+        self.progressBackgroundColor = background
+        self.progress = progress
+        self.clockwise = clockwise
+        
+    }
+    
+    public func startProgressAnimate() {
+        circleLayer.animate()
+    }
+}
---
+++ CodeStack/Presentation/MyPage/View/StatusView.swift
@@ -0,0 +1,127 @@
+//
+//  StatusView.swift
+//  CodeStack
+//
+//  Created by 박형환 on 2023/06/25.
+//
+
+import UIKit
+import SnapKit
+
+
+class StatusView: UIView{
+    
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Solved Problems"
+        label.font = UIFont.preferredFont(forTextStyle: .caption1)
+        label.textColor = .lightGray
+        label.textAlignment = .left
+        return label
+    }()
+
+    
+     let circleProgressView: CircleProgressView = {
+        let view = CircleProgressView()
+        view.progress = 0.3
+        return view
+    }()
+    
+    private let statusViewEasy: ProgressView = {
+        let view = ProgressView(progressViewStyle: .bar)
+        view.tintColor = .systemGreen
+        view.backgroundColor = UIColor.green.withAlphaComponent(0.3)
+        return view
+    }()
+    
+    private let statusViewMedium: ProgressView = {
+        let view = ProgressView(progressViewStyle: .bar)
+        view.tintColor = .systemYellow
+        view.backgroundColor = UIColor.yellow.withAlphaComponent(0.3)
+        return view
+    }()
+    
+    private let statusViewHard: ProgressView = {
+        let view = ProgressView(progressViewStyle: .bar)
+        view.tintColor = .systemRed
+        view.backgroundColor = UIColor.red.withAlphaComponent(0.3)
+        return view
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layoutConfigure()
+        self.backgroundColor = .tertiarySystemBackground
+        
+        circleProgressView.progressColor = .juhwang
+        circleProgressView.progressBackgroundColor = .gray
+        circleProgressView.progressWidth = 8
+        circleProgressView.progress = 0.5
+        circleProgressView.clockwise = false
+    }
+    
+    
+    func settingProgressViewAnimation(_ easy: Float,
+                                      _ medium: Float,
+                                      _ hard: Float){
+        self.statusViewEasy.setProgress(easy, animated: true)
+        self.statusViewMedium.setProgress(medium, animated: true)
+        self.statusViewHard.setProgress(hard, animated: true)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("required init fatalError")
+        
+    }
+    
+    private func layoutConfigure() {
+        
+        let statusBarSpacingTop: CGFloat = 35
+        let circleWidthHeight: CGFloat = 100
+        
+        let bottomInsetFromSuperView: CGFloat = 30
+        [titleLabel,
+         circleProgressView,
+         statusViewEasy,
+         statusViewMedium,
+         statusViewHard].forEach{
+            self.addSubview($0)
+        }
+        
+        
+        titleLabel.snp.makeConstraints{
+            $0.top.equalToSuperview().inset(12)
+            $0.trailing.leading.equalToSuperview().inset(12)
+        }
+        
+        circleProgressView.snp.makeConstraints{
+            $0.width.height.equalTo(circleWidthHeight)
+            $0.centerY.equalTo(statusViewMedium.snp.centerY)
+            $0.leading.equalToSuperview().inset(12)
+        }
+        
+        statusViewEasy.snp.makeConstraints{
+            $0.top.equalTo(statusBarSpacingTop + 12)
+            $0.leading.equalTo(circleProgressView.snp.trailing).offset(20)
+            $0.height.equalTo(10)
+            $0.trailing.equalToSuperview().inset(20)
+        }
+        
+        statusViewMedium.snp.makeConstraints{
+            $0.top.equalTo(statusViewEasy.snp.bottom).offset(statusBarSpacingTop)
+            $0.leading.equalTo(circleProgressView.snp.trailing).offset(20)
+            $0.height.equalTo(10)
+            $0.trailing.equalToSuperview().inset(20)
+        }
+        statusViewHard.snp.makeConstraints{
+            $0.top.equalTo(statusViewMedium.snp.bottom).offset(statusBarSpacingTop)
+            $0.leading.equalTo(circleProgressView.snp.trailing).offset(20)
+            $0.height.equalTo(10)
+            $0.trailing.equalToSuperview().inset(20)
+            $0.bottom.equalToSuperview().offset(-bottomInsetFromSuperView)
+        }
+    }
+    
+}
