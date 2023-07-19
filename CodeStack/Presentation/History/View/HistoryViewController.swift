9e17de23afb37e93d381a88dcfdfe64ca7b28ec9
pHH <gudghks56@gmail.com>
2023-07-19 22:06:29 +0900

    feat: History Binding 추가

--- CodeStack/Presentation/History/View/HistoryViewController.swift
+++ CodeStack/Presentation/History/View/HistoryViewController.swift
@@ -9,36 +9,122 @@ import UIKit
 import SnapKit
 import RxSwift
 import RxCocoa
+import RxGesture
 
 class HistoryViewController: UIViewController {
     
+    var historyViewModel: HistoryViewModel?
     
-    private lazy var container: ContainerView = {
+    static func create(with dependency: HistoryViewModel) -> HistoryViewController{
+        let history = HistoryViewController()
+        history.historyViewModel = dependency
+        return history
+    }
+    
+    
+    private let container: ContainerView = {
         let view = ContainerView(frame: .zero)
         view.backgroundColor = .tertiarySystemBackground
         return view
     }()
     
-    var historySegmentList: HistorySegmentedControl = {
+    private let historySegmentList: HistorySegmentedControl = {
         let seg = HistorySegmentedControl(frame: .zero)
         return seg
     }()
+    
+    private let historyList: UITableView = {
+        let table = UITableView(frame: .zero)
+        table.register(HistoryCell.self, forCellReuseIdentifier: HistoryCell.identifier)
+        return table
+    }()
+    
+    var _viewDidLoad = PublishSubject<Void>()
 
     var disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        layoutConfigure()
+        binding()
+        _viewDidLoad.onNext(())
+    }
+    
+    func binding(){
+        
+        let rightGesture = historyList.rx.gesture(.swipe(direction: .right))
+            .withUnretained(self)
+            .map{ vc, _ in
+                if vc.historySegmentList.selectedSegmentIndex != 0{
+                    vc.historySegmentList.selectedSegmentIndex -= 1
+                }
+                return vc.historySegmentList.selectedSegmentIndex
+            }.asDriver(onErrorJustReturn: 0)
+        
+        
+        let leftGesture = historyList.rx.gesture(.swipe(direction: .left))
+            .withUnretained(self)
+            .map{ vc, _ in
+                if vc.historySegmentList.selectedSegmentIndex != 4{
+                    vc.historySegmentList.selectedSegmentIndex += 1
+                }
+                return vc.historySegmentList.selectedSegmentIndex
+            }.asDriver(onErrorJustReturn: 0)
+        
+        
+        let historyDriver = historySegmentList.rx.selectedSegmentIndex.asDriver()
+        
+        let historySegmentUtil = Driver.merge([rightGesture,leftGesture,historyDriver])
+        
+        historySegmentUtil
+            .drive(with: self,
+                   onNext: { vm, _ in
+                vm.historySegmentList.setNeedsDisplay()
+            }).disposed(by: disposeBag)
+        
+        
+        let currentSegment
+        =
+        historySegmentUtil.map{ value in
+            SegType.switchSegType(value: SegType.Value(rawValue: value) ?? SegType.Value.all)
+        }.asSignal(onErrorJustReturn: .all)
+        
+        
+        guard let output = historyViewModel?
+            .transform(input:
+                        HistoryViewModel.Input(viewDidLoad: _viewDidLoad.asSignal(onErrorJustReturn: ()),
+                                               currentSegment: currentSegment)) else { return }
+        
+        output.historyData
+            .drive(historyList.rx.items(cellIdentifier: HistoryCell.identifier,
+                                        cellType: HistoryCell.self))
+        { index , value , cell in
+            cell.publishRelay.accept(value)
+        }.disposed(by: disposeBag)
+        
+    }
+}
+
+
+//MARK: - Layout configure
+extension HistoryViewController{
+    
+    func layoutConfigure(){
         self.view.backgroundColor = .tertiarySystemBackground
         
         view.addSubview(container)
+        view.addSubview(historyList)
         container.addSubview(historySegmentList)
         historySegmentList.selectedSegmentIndex = 0
         
-        historySegmentList.rx.selectedSegmentIndex
-            .subscribe(onNext: { [weak self] value in
-                guard let self else {return}
-                self.historySegmentList.setNeedsDisplay()
-            }).disposed(by: disposeBag)
+        
+        
+        historyList.snp.makeConstraints{
+            $0.top.equalTo(container.snp.bottom).offset(12)
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
         
         container.snp.makeConstraints{
             $0.height.equalTo(44)
@@ -54,6 +140,13 @@ class HistoryViewController: UIViewController {
             $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-50)
             $0.bottom.equalToSuperview()
         }
-        
+    }
+}
+
+extension HistoryViewController: UIGestureRecognizerDelegate{
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer,
+                           shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer)
+    -> Bool {
+        return false
     }
 }
