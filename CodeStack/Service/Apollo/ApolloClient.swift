8f0e2e3f4afbcc1e5701e9559e606cec75022b9a
pHH <gudghks56@gmail.com>
2023-07-24 22:04:33 +0900

    feta: Apollo 클라이언트 싱글톤, Rx 추가

---
+++ CodeStack/Service/Apollo/ApolloClient.swift
@@ -0,0 +1,62 @@
+//
+//  Client.swift
+//  CodeStack
+//
+//  Created by 박형환 on 2023/07/23.
+//
+
+import Foundation
+import Apollo
+import RxSwift
+import CodestackAPI
+
+
+
+
+final class ApolloAPI{
+    
+    static var shared = ApolloAPI()
+    
+    private let client: ApolloClient
+    
+    private let baseURL = API.baseURL
+    
+    private var accessToken: String = KeychainItem.currentAccessToken
+    
+    private init() {
+        let configuration: URLSessionConfiguration = .default
+        configuration.httpAdditionalHeaders = ["Authorization": "Bearer \(accessToken)"]
+        configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        
+        let sessionClient = URLSessionClient(
+            sessionConfiguration: configuration,
+            callbackQueue: .main
+        )
+        
+        let store = ApolloStore(cache: InMemoryNormalizedCache())
+        
+        self.client = ApolloClient(
+            networkTransport: RequestChainNetworkTransport(
+                interceptorProvider: DefaultInterceptorProvider(
+                    client: sessionClient,
+                    shouldInvalidateClientOnDeinit: true,
+                    store: store
+                ),
+                endpointURL: baseURL
+            ),
+            store: store
+        )
+    }
+    
+    func fetch<Query: GraphQLQuery>(
+        query: Query,
+        cachePolicy: CachePolicy = .default,
+        queue: DispatchQueue = DispatchQueue.main) -> Maybe<Query.Data>
+    {
+        return self.client.rx
+            .fetch(query: query,
+                   cachePolicy: cachePolicy,
+                   queue: queue)
+    }
+}
+
