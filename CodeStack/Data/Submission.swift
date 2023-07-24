6590ed2ead337f2cb988353da07182c24c453b23
pHH <gudghks56@gmail.com>
2023-07-24 22:25:51 +0900

    test: 임시 리퀘스트 함수 추가 및 임시 생성자 추가

--- CodeStack/Data/Submission.swift
+++ CodeStack/Data/Submission.swift
@@ -12,28 +12,34 @@ struct SubmissionPagedResult: Codable{
     let content: [Submission]
     let pageInfo: _PageInfo
 }
-struct Submission: Codable{
-    let id: Int?
-    let sourceCode: String?
-    let problem: _Problem?
-    let member: _Member?
-    let language: Language?
-    let cpuTime: Int?
-    let memoryUsage: Int?
-    let statusCode: String?
-    let createdAt: String?
 
+struct Submission: Codable{
+    var id: Int?
+    var sourceCode: String?
+    var problem: _Problem?
+    var member: _Member?
+    var language: Language?
+    var cpuTime: Int?
+    var memoryUsage: Int?
+    var statusCode: String?
+    var createdAt: String?
     
-    init(){
+    
+    init( _problem: _Problem){
         id = nil
         sourceCode = nil
-        problem = nil
+        problem = _problem
         member = nil
         language = nil
         cpuTime = nil
         memoryUsage = nil
         statusCode = nil
         createdAt = nil
+    }
+    
+    init(_problem: _Problem,status code: String){
+        self.init(_problem: _problem)
+        self.statusCode = code
     }
     
     init(id: Int, sourceCode: String, problem: _Problem, member: _Member, language: Language, cpuTime: Int, memoryUsage: Int, statusCode: String, createdAt: String) {
@@ -53,98 +59,37 @@ struct Submission: Codable{
 
 extension Submission{
     
-    static func dummy() -> [Self]{
-        return []
+    static func convertSolveStatus(_ code: String) -> SolveStatus {
+        switch code{
+        case "fail":
+            return .fail
+        case "success":
+            return .solve
+        case "temp":
+            return .temp
+        case "favorite":
+            return .favorite
+        default:
+            return .none
+        }
     }
     
+    static func dummy(type: SolveStatus? = nil) -> [Self]{
+        
+        let dummy = [Submission(_problem: _Problem(title: "hello"),status: "fail"),
+                     Submission(_problem: _Problem(title: "my"),status: "success"),
+                     Submission(_problem: _Problem(title: "name"),status: "temp"),
+                     Submission(_problem: _Problem(title: "is"),status: "success"),
+                     Submission(_problem: _Problem(title: "hyeong"),status: "success"),
+                     Submission(_problem: _Problem(title: "hwan"),status: "temp"),
+                     Submission(_problem: _Problem(title: "zzz"),status: "fail"),
+                     Submission(_problem: _Problem(title: "hahaha"),status: "success"),
+                     Submission(_problem: _Problem(title: "no~"),status: "fail"),
+                     Submission(_problem: _Problem(title: "no~"),status: "favorite")]
+        if let type {
+            return dummy.filter{ Submission.convertSolveStatus($0.statusCode ?? "") == type }
+        }else{
+            return dummy
+        }
+    }
 }
-//        [Submission(id: 12,
-//                              sourceCode: "asdfasdf",
-//                              problem: Problem(id: 3, title: "hellow world!", context: "설명", maxCpuTime: 10000, maxMemory: 10000, submission: 3, accepted: 2,
-//                                               tags: [Tag(id: "12", name: "df")],
-//                                               languages: [Language(id: 1, name: "C++", _extension: "cpp"),Language(id: 1, name: "java", _extension: "java")]),
-//                              member: Member(username: "park", nickname: "hwan", email: "gudghks56", profileImage: "star", problemCalendar: [], year: "2023"),
-//                              language: Language(id: 1, name: "C++", _extension: "cpp"),
-//                              cpuTime: 2324,
-//                              memoryUsage: 1123234,
-//                              statusCode: "201",
-//                              createdAt: Date().description),
-//                   Submission(id: 12,
-//                                         sourceCode: "asdfasdf",
-//                                         problem: Problem(id: 3, title: "DFS!", context: "설명", maxCpuTime: 10000, maxMemory: 10000, submission: 3, accepted: 2,
-//                                                          tags: [Tag(id: "12", name: "df")],
-//                                                          languages: [Language(id: 1, name: "C++", _extension: "cpp"),Language(id: 1, name: "java", _extension: "java")]),
-//                                         member: Member(username: "park", nickname: "hwan", email: "gudghks56", profileImage: "star", problemCalendar: [], year: "2023"),
-//                                         language: Language(id: 1, name: "C++", _extension: "cpp"),
-//                                         cpuTime: 2324,
-//                                         memoryUsage: 1123234,
-//                                         statusCode: "201",
-//                                         createdAt: Date().description),
-//                   Submission(id: 12,
-//                                         sourceCode: "asdfasdf",
-//                                         problem: Problem(id: 3, title: "미로 탐색 하기!", context: "설명", maxCpuTime: 10000, maxMemory: 10000, submission: 3, accepted: 2,
-//                                                          tags: [Tag(id: "12", name: "df")],
-//                                                          languages: [Language(id: 1, name: "C++", _extension: "cpp"),Language(id: 1, name: "java", _extension: "java")]),
-//                                         member: Member(username: "park", nickname: "hwan", email: "gudghks56", profileImage: "star", problemCalendar: [], year: "2023"),
-//                                         language: Language(id: 1, name: "C++", _extension: "cpp"),
-//                                         cpuTime: 2324,
-//                                         memoryUsage: 1123234,
-//                                         statusCode: "201",
-//                                         createdAt: Date().description),
-//                   Submission(id: 12,
-//                                         sourceCode: "asdfasdf",
-//                                         problem: Problem(id: 3, title: "해보자 함!", context: "설명", maxCpuTime: 10000, maxMemory: 10000, submission: 3, accepted: 2,
-//                                                          tags: [Tag(id: "12", name: "df")],
-//                                                          languages: [Language(id: 1, name: "C++", _extension: "cpp"),Language(id: 1, name: "java", _extension: "java")]),
-//                                         member: Member(username: "park", nickname: "hwan", email: "gudghks56", profileImage: "star", problemCalendar: [], year: "2023"),
-//                                         language: Language(id: 1, name: "C++", _extension: "cpp"),
-//                                         cpuTime: 2324,
-//                                         memoryUsage: 1123234,
-//                                         statusCode: "201",
-//                                         createdAt: Date().description),
-//                   Submission(id: 12,
-//                                         sourceCode: "asdfasdf",
-//                                         problem: Problem(id: 3, title: "달려라 허니!", context: "설명", maxCpuTime: 10000, maxMemory: 10000, submission: 3, accepted: 2,
-//                                                          tags: [Tag(id: "12", name: "df")],
-//                                                          languages: [Language(id: 1, name: "C++", _extension: "cpp"),Language(id: 1, name: "java", _extension: "java")]),
-//                                         member: Member(username: "park", nickname: "hwan", email: "gudghks56", profileImage: "star", problemCalendar: [], year: "2023"),
-//                                         language: Language(id: 1, name: "C++", _extension: "cpp"),
-//                                         cpuTime: 2324,
-//                                         memoryUsage: 1123234,
-//                                         statusCode: "201",
-//                                         createdAt: Date().description),
-//                   Submission(id: 12,
-//                                         sourceCode: "asdfasdf",
-//                                         problem: Problem(id: 3, title: "소방대원 구출작전!", context: "설명", maxCpuTime: 10000, maxMemory: 10000, submission: 3, accepted: 2,
-//                                                          tags: [Tag(id: "12", name: "df")],
-//                                                          languages: [Language(id: 1, name: "C++", _extension: "cpp"),Language(id: 1, name: "java", _extension: "java")]),
-//                                         member: Member(username: "park", nickname: "hwan", email: "gudghks56", profileImage: "star", problemCalendar: [], year: "2023"),
-//                                         language: Language(id: 1, name: "C++", _extension: "cpp"),
-//                                         cpuTime: 2324,
-//                                         memoryUsage: 1123234,
-//                                         statusCode: "201",
-//                                         createdAt: Date().description),
-//                   Submission(id: 12,
-//                                         sourceCode: "asdfasdf",
-//                                         problem: Problem(id: 3, title: "카카오 배달의 민족 !", context: "설명", maxCpuTime: 10000, maxMemory: 10000, submission: 3, accepted: 2,
-//                                                          tags: [Tag(id: "12", name: "df")],
-//                                                          languages: [Language(id: 1, name: "C++", _extension: "cpp"),Language(id: 1, name: "java", _extension: "java")]),
-//                                         member: Member(username: "park", nickname: "hwan", email: "gudghks56", profileImage: "star", problemCalendar: [], year: "2023"),
-//                                         language: Language(id: 1, name: "C++", _extension: "cpp"),
-//                                         cpuTime: 2324,
-//                                         memoryUsage: 1123234,
-//                                         statusCode: "201",
-//                                         createdAt: Date().description),
-//                   Submission(id: 12,
-//                                         sourceCode: "asdfasdf",
-//                                         problem: Problem(id: 3, title: "이진 트리 구현!", context: "설명", maxCpuTime: 10000, maxMemory: 10000, submission: 3, accepted: 2,
-//                                                          tags: [Tag(id: "12", name: "df")],
-//                                                          languages: [Language(id: 1, name: "C++", _extension: "cpp"),Language(id: 1, name: "java", _extension: "java")]),
-//                                         member: Member(username: "park", nickname: "hwan", email: "gudghks56", profileImage: "star", problemCalendar: [], year: "2023"),
-//                                         language: Language(id: 1, name: "C++", _extension: "cpp"),
-//                                         cpuTime: 2324,
-//                                         memoryUsage: 1123234,
-//                                         statusCode: "201",
-//                                         createdAt: Date().description)]
-//    }\
-
