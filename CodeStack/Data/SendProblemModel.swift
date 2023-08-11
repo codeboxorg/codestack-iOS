508d105785a17b6b1ff28d20fa103ab942d178aa
pHH <gudghks56@gmail.com>
2023-08-11 20:49:20 +0900

    feat: 문제 Mutation send Model

---
+++ CodeStack/Data/SendProblemModel.swift
@@ -0,0 +1,16 @@
+//
+//  SendProblemModel.swift
+//  CodeStack
+//
+//  Created by 박형환 on 2023/08/04.
+//
+
+import Foundation
+
+
+struct SendProblemModel{
+    
+    let problemID: ID
+    let sourceCode: String
+    let languageID: String
+}
