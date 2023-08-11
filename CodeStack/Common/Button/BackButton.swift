a8e9cb280c6915a5d673fd0fe2b0758e5dca2199
pHH <gudghks56@gmail.com>
2023-08-11 20:53:39 +0900

    feat: 공통 버튼 추가

---
+++ CodeStack/Common/Button/BackButton.swift
@@ -0,0 +1,22 @@
+//
+//  BackButton.swift
+//  CodeStack
+//
+//  Created by 박형환 on 2023/08/07.
+//
+
+import UIKit
+
+
+class BackButton: UIButton{
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    required init?(coder: NSCoder) {
+        fatalError("required init fatalError")
+        
+    }
+}
---
+++ CodeStack/Common/Button/NumberButton.swift
@@ -0,0 +1,75 @@
+//
+//  NumberButton.swift
+//  CodeStack
+//
+//  Created by 박형환 on 2023/08/07.
+//
+
+import UIKit
+import SnapKit
+
+class NumberButton: UIButton{
+    
+    private let container: UIView = {
+        let number = UIView()
+        number.layer.borderColor = UIColor.sky_blue.cgColor
+        number.layer.borderWidth = 1
+        number.layer.cornerRadius = 5
+        return number
+    }()
+    
+    private let numberLabel: UILabel = {
+        let label = UILabel()
+        return label.headLineLabel(size: 16, text: "1", color: .label)
+    }()
+    
+    private let textlabel: UILabel = {
+        let label = UILabel()
+        return label.headLineLabel(size: 16, text: "", color: .label)
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layoutConfigure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("required init fatalError")
+        
+    }
+    
+    func settingNumber(for index: Int){
+        numberLabel.text = "\(index)"
+    }
+    
+    func settingText(for text: String){
+        textlabel.text = text
+    }
+    
+    private func layoutConfigure(){
+        
+        [container,
+         textlabel]
+            .forEach{
+                self.addSubview($0)
+        }
+        
+        container.addSubview(numberLabel)
+        
+        container.snp.makeConstraints { make in
+            make.top.leading.bottom.equalToSuperview().inset(5)
+            make.width.height.equalTo(30)
+        }
+        
+        numberLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        textlabel.snp.makeConstraints { make in
+            make.leading.equalTo(container.snp.trailing).offset(5)
+            make.centerY.equalTo(container.snp.centerY)
+            make.trailing.equalToSuperview().inset(5)
+        }
+    }
+}
