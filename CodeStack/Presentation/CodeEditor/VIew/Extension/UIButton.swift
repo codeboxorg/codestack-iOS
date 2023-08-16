a72ff17a46d1fa7da4429383403c36cb33cc8f80
pHH <gudghks56@gmail.com>
2023-08-16 23:56:03 +0900

    feat: 문제 제출 결과 뷰 생성

---
+++ CodeStack/Presentation/CodeEditor/VIew/Extension/UIButton.swift
@@ -0,0 +1,8 @@
+//
+//  UIButton.swift
+//  CodeStack
+//
+//  Created by 박형환 on 2023/08/16.
+//
+
+import Foundation
---
+++ CodeStack/Presentation/CodeEditor/VIew/SubmissionResultView.swift
@@ -0,0 +1,8 @@
+//
+//  SubmissionResultView.swift
+//  CodeStack
+//
+//  Created by 박형환 on 2023/08/16.
+//
+
+import Foundation
