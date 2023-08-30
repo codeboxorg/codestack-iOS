a15282326bee8c2adda191e79941a71d30edd42d
pHH <gudghks56@gmail.com>
2023-08-30 20:36:30 +0900

    fix: segment 블루 인디케이터 높이 Solvestatus Enum 이동

--- CodeStack/Presentation/History/View/HistorySegmentedControl.swift
+++ CodeStack/Presentation/History/View/HistorySegmentedControl.swift
@@ -8,36 +8,6 @@
 import UIKit
 
 
-enum SegType: String,CaseIterable{
-    case favorite = "즐겨찾기"
-    case tempSave = "임시저장"
-    case success = "성공"
-    case failure = "실패"
-    case all = "전체"
-    
-    enum Value: Int{
-        case favorite = 0
-        case tempSave = 1
-        case success = 2
-        case failure = 3
-        case all = 4
-    }
-    
-    static func switchSegType(value: Value) -> SolveStatus{
-        switch value{
-        case .favorite:
-            return .favorite
-        case .tempSave:
-            return .temp
-        case .success:
-            return .solve
-        case .failure:
-            return .fail
-        default:
-            return .none
-        }
-    }
-}
 
 class HistorySegmentedControl: UISegmentedControl{
     
@@ -86,7 +56,7 @@ class HistorySegmentedControl: UISegmentedControl{
         
         let layer = UIBezierPath()
         
-        layer.lineWidth = 2.0
+        layer.lineWidth = 5.0
         layer.move(to: CGPoint(x: xPosition, y: yPosition))
         layer.addLine(to: CGPoint(x: xPosition + selectedSegWidth , y: yPosition))
         UIColor.sky_blue.setStroke()
