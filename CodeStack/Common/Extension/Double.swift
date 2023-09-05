32b7b9e84d0e78ea6aa58ad1d41489c2ace4e063
pHH <gudghks56@gmail.com>
2023-09-05 23:43:01 +0900

    feat: Double, TableView, Image 관련 Extension TableView: 페이지 네이션 할때 바텀의 인디케이터 추가 삭제 함수 Double: 비율 구하는 함수 Image: 이미지에 컬러 입히는 함수 추가

--- CodeStack/Common/Extension/Double.swift
+++ CodeStack/Common/Extension/Double.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+
+
+extension Double{
+    
+    /// 비율 구하는 함수
+    /// - Parameters:
+    ///   - decimal: 정수 1
+    ///   - second: 정수 2
+    /// - Returns: 비율
+    func toRate(decimal count: Int = 2, second: Double) -> Double{
+        let value = ((second) / (self)) * 100.0
+        return Double(String(format: "%.\(count)f", value))!
+    }
+}
