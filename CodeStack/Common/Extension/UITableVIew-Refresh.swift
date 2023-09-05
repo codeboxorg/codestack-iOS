5fa971299cbb7a89a339c79071a5317fde53bbf0
pHH <gudghks56@gmail.com>
2023-09-05 23:43:01 +0900

    feat: Double, TableView, Image 관련 Extension TableView: 페이지 네이션 할때 바텀의 인디케이터 추가 삭제 함수 Double: 비율 구하는 함수 Image: 이미지에 컬러 입히는 함수 추가

--- CodeStack/Common/Extension/UITableVIew-Refresh.swift
+++ CodeStack/Common/Extension/UITableVIew-Refresh.swift
@@ -5,4 +5,36 @@
 //  Created by 박형환 on 2023/08/29.
 //
 
-import Foundation
+import UIKit
+
+
+extension UITableView{
+    
+    private var animationDuration: Double {
+        0.2
+    }
+    
+    private var bottomView: CustomBottomView {
+        CustomBottomView()
+    }
+    
+    func addBottomRefresh(width: CGFloat, height: CGFloat = 50){
+        let bottomView = CustomBottomView()
+        bottomView.frame = CGRect(x: 0, y: 0, width: width, height: height)
+        self.tableFooterView = bottomView
+        
+        UIView.animate(withDuration: animationDuration) {
+            self.contentInset.bottom = 50
+        }
+    }
+    
+    func removeBottomRefresh(){
+        DispatchQueue.main.async { [weak self] in
+            if let self = self{
+                UIView.animate(withDuration: self.animationDuration) {
+                    self.tableFooterView = nil
+                }
+            }
+        }
+    }
+}
