61abda2457823b60b700f184d4fa13641026765a
pHH <gudghks56@gmail.com>
2023-09-05 23:46:09 +0900

    feat: 코드스택 API 쿼리 추가

---
+++ codestackAPI/Sources/Operations/Queries/GetMeSubmissionsQuery.graphql.swift
@@ -0,0 +1,130 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+@_exported import ApolloAPI
+
+public class GetMeSubmissionsQuery: GraphQLQuery {
+  public static let operationName: String = "GetMeSubmissions"
+  public static let document: ApolloAPI.DocumentType = .notPersisted(
+    definition: .init(
+      #"""
+      query GetMeSubmissions {
+        getMe {
+          __typename
+          submissions {
+            __typename
+            id
+            statusCode
+            sourceCode
+            cpuTime
+            memoryUsage
+            createdAt
+            problem {
+              __typename
+              id
+              title
+            }
+            language {
+              __typename
+              name
+            }
+          }
+        }
+      }
+      """#
+    ))
+
+  public init() {}
+
+  public struct Data: CodestackAPI.SelectionSet {
+    public let __data: DataDict
+    public init(_dataDict: DataDict) { __data = _dataDict }
+
+    public static var __parentType: ApolloAPI.ParentType { CodestackAPI.Objects.Query }
+    public static var __selections: [ApolloAPI.Selection] { [
+      .field("getMe", GetMe.self),
+    ] }
+
+    public var getMe: GetMe { __data["getMe"] }
+
+    /// GetMe
+    ///
+    /// Parent Type: `Member`
+    public struct GetMe: CodestackAPI.SelectionSet {
+      public let __data: DataDict
+      public init(_dataDict: DataDict) { __data = _dataDict }
+
+      public static var __parentType: ApolloAPI.ParentType { CodestackAPI.Objects.Member }
+      public static var __selections: [ApolloAPI.Selection] { [
+        .field("__typename", String.self),
+        .field("submissions", [Submission].self),
+      ] }
+
+      public var submissions: [Submission] { __data["submissions"] }
+
+      /// GetMe.Submission
+      ///
+      /// Parent Type: `Submission`
+      public struct Submission: CodestackAPI.SelectionSet {
+        public let __data: DataDict
+        public init(_dataDict: DataDict) { __data = _dataDict }
+
+        public static var __parentType: ApolloAPI.ParentType { CodestackAPI.Objects.Submission }
+        public static var __selections: [ApolloAPI.Selection] { [
+          .field("__typename", String.self),
+          .field("id", CodestackAPI.ID.self),
+          .field("statusCode", String?.self),
+          .field("sourceCode", String.self),
+          .field("cpuTime", Double?.self),
+          .field("memoryUsage", Double?.self),
+          .field("createdAt", CodestackAPI.DateTime.self),
+          .field("problem", Problem.self),
+          .field("language", Language.self),
+        ] }
+
+        public var id: CodestackAPI.ID { __data["id"] }
+        public var statusCode: String? { __data["statusCode"] }
+        public var sourceCode: String { __data["sourceCode"] }
+        public var cpuTime: Double? { __data["cpuTime"] }
+        public var memoryUsage: Double? { __data["memoryUsage"] }
+        public var createdAt: CodestackAPI.DateTime { __data["createdAt"] }
+        public var problem: Problem { __data["problem"] }
+        public var language: Language { __data["language"] }
+
+        /// GetMe.Submission.Problem
+        ///
+        /// Parent Type: `Problem`
+        public struct Problem: CodestackAPI.SelectionSet {
+          public let __data: DataDict
+          public init(_dataDict: DataDict) { __data = _dataDict }
+
+          public static var __parentType: ApolloAPI.ParentType { CodestackAPI.Objects.Problem }
+          public static var __selections: [ApolloAPI.Selection] { [
+            .field("__typename", String.self),
+            .field("id", CodestackAPI.ID.self),
+            .field("title", String.self),
+          ] }
+
+          public var id: CodestackAPI.ID { __data["id"] }
+          public var title: String { __data["title"] }
+        }
+
+        /// GetMe.Submission.Language
+        ///
+        /// Parent Type: `Language`
+        public struct Language: CodestackAPI.SelectionSet {
+          public let __data: DataDict
+          public init(_dataDict: DataDict) { __data = _dataDict }
+
+          public static var __parentType: ApolloAPI.ParentType { CodestackAPI.Objects.Language }
+          public static var __selections: [ApolloAPI.Selection] { [
+            .field("__typename", String.self),
+            .field("name", String.self),
+          ] }
+
+          public var name: String { __data["name"] }
+        }
+      }
+    }
+  }
+}
---
+++ codestackAPI/Sources/Operations/Queries/GetSearchproblemsQuery.graphql.swift
@@ -0,0 +1,129 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+@_exported import ApolloAPI
+
+public class GetSearchproblemsQuery: GraphQLQuery {
+  public static let operationName: String = "GetSearchproblems"
+  public static let document: ApolloAPI.DocumentType = .notPersisted(
+    definition: .init(
+      #"""
+      query GetSearchproblems($keyword: String!, $limit: Int! = 10, $order: String! = "asc", $sort: String! = "id") {
+        getSearchProblems(keyword: $keyword, limit: $limit, order: $order, sort: $sort) {
+          __typename
+          content {
+            __typename
+            id
+            title
+            context
+            languages {
+              __typename
+              id
+              name
+              extension
+            }
+          }
+        }
+      }
+      """#
+    ))
+
+  public var keyword: String
+  public var limit: Int
+  public var order: String
+  public var sort: String
+
+  public init(
+    keyword: String,
+    limit: Int = 10,
+    order: String = "asc",
+    sort: String = "id"
+  ) {
+    self.keyword = keyword
+    self.limit = limit
+    self.order = order
+    self.sort = sort
+  }
+
+  public var __variables: Variables? { [
+    "keyword": keyword,
+    "limit": limit,
+    "order": order,
+    "sort": sort
+  ] }
+
+  public struct Data: CodestackAPI.SelectionSet {
+    public let __data: DataDict
+    public init(_dataDict: DataDict) { __data = _dataDict }
+
+    public static var __parentType: ApolloAPI.ParentType { CodestackAPI.Objects.Query }
+    public static var __selections: [ApolloAPI.Selection] { [
+      .field("getSearchProblems", GetSearchProblems.self, arguments: [
+        "keyword": .variable("keyword"),
+        "limit": .variable("limit"),
+        "order": .variable("order"),
+        "sort": .variable("sort")
+      ]),
+    ] }
+
+    public var getSearchProblems: GetSearchProblems { __data["getSearchProblems"] }
+
+    /// GetSearchProblems
+    ///
+    /// Parent Type: `ProblemPage`
+    public struct GetSearchProblems: CodestackAPI.SelectionSet {
+      public let __data: DataDict
+      public init(_dataDict: DataDict) { __data = _dataDict }
+
+      public static var __parentType: ApolloAPI.ParentType { CodestackAPI.Objects.ProblemPage }
+      public static var __selections: [ApolloAPI.Selection] { [
+        .field("__typename", String.self),
+        .field("content", [Content]?.self),
+      ] }
+
+      public var content: [Content]? { __data["content"] }
+
+      /// GetSearchProblems.Content
+      ///
+      /// Parent Type: `Problem`
+      public struct Content: CodestackAPI.SelectionSet {
+        public let __data: DataDict
+        public init(_dataDict: DataDict) { __data = _dataDict }
+
+        public static var __parentType: ApolloAPI.ParentType { CodestackAPI.Objects.Problem }
+        public static var __selections: [ApolloAPI.Selection] { [
+          .field("__typename", String.self),
+          .field("id", CodestackAPI.ID.self),
+          .field("title", String.self),
+          .field("context", String.self),
+          .field("languages", [Language].self),
+        ] }
+
+        public var id: CodestackAPI.ID { __data["id"] }
+        public var title: String { __data["title"] }
+        public var context: String { __data["context"] }
+        public var languages: [Language] { __data["languages"] }
+
+        /// GetSearchProblems.Content.Language
+        ///
+        /// Parent Type: `Language`
+        public struct Language: CodestackAPI.SelectionSet {
+          public let __data: DataDict
+          public init(_dataDict: DataDict) { __data = _dataDict }
+
+          public static var __parentType: ApolloAPI.ParentType { CodestackAPI.Objects.Language }
+          public static var __selections: [ApolloAPI.Selection] { [
+            .field("__typename", String.self),
+            .field("id", CodestackAPI.ID.self),
+            .field("name", String.self),
+            .field("extension", String.self),
+          ] }
+
+          public var id: CodestackAPI.ID { __data["id"] }
+          public var name: String { __data["name"] }
+          public var `extension`: String { __data["extension"] }
+        }
+      }
+    }
+  }
+}
