a845e368afb923fef681c06790ce531f2fbf9e47
pHH <gudghks56@gmail.com>
2023-09-10 00:42:29 +0900

    feat: 프로필 이미지 디테일 뷰

---
+++ CodeStack/Presentation/MyPage/View/ProfileImageViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ProfileImageViewController.swift
+//  CodeStack
+//
+//  Created by 박형환 on 2023/09/09.
+//
+
+import UIKit
+
+
+class ProfileImageViewController: UIViewController {
+    
+    
+    var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "person")
+        imageView.tintColor = UIColor.gray
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 12
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.layer.borderWidth = 1
+        return imageView
+    }()
+    
+    static func create(with: UIImage?) -> ProfileImageViewController {
+        let profile = ProfileImageViewController()
+        profile.imageView.image = with
+        return profile
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(imageView)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+}
+
