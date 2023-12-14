308b8ce1af0b2a90e70f6572995c364cd8d1404e
pHH <gudghks56@gmail.com>
2023-12-14 20:00:29 +0900

    feat: Domain module 초기 셋팅

---
+++ Projects/CodestackApp/Sources/Model/Model+Mapping/FavoriteProblem.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteSubmission.swift
+//  CodeStack
+//
+//  Created by 박형환 on 11/22/23.
+//
+
+import Foundation
+
+public struct FavoriteProblem {
+    let problemID: String
+    let problmeTitle: String
+    let createdAt: Date
+}
+
+
