b24808e125b7626d88a34f9036846cdabd512ae5
pHH <gudghks56@gmail.com>
2023-12-14 15:05:48 +0900

    feat: Core Data MO Mapping 코드 추가

---
+++ Projects/CodestackApp/Sources/Data/Repository/MO/Class/ProblemSubmissionStateMO+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ProblemSubmissionStateMO+CoreDataClass.swift
+//  CodeStack
+//
+//  Created by 박형환 on 11/28/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ProblemSubmissionStateMO)
+public class ProblemSubmissionStateMO: NSManagedObject {
+
+}
---
+++ Projects/CodestackApp/Sources/Data/Repository/MO/SubmissionCalendarMO+CoreDataProperties.swift
@@ -0,0 +1,59 @@
+//
+//  SubmissionCalendarMO+CoreDataProperties.swift
+//  CodeStack
+//
+//  Created by 박형환 on 12/2/23.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension SubmissionCalendarMO {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<SubmissionCalendarMO> {
+        return NSFetchRequest<SubmissionCalendarMO>(entityName: "SubmissionCalendar")
+    }
+
+    @NSManaged public var submission: NSSet?
+
+}
+
+// MARK: Generated accessors for submission
+extension SubmissionCalendarMO {
+
+    @objc(addSubmissionObject:)
+    @NSManaged public func addToSubmission(_ value: SubmissionMO)
+
+    @objc(removeSubmissionObject:)
+    @NSManaged public func removeFromSubmission(_ value: SubmissionMO)
+
+    @objc(addSubmission:)
+    @NSManaged public func addToSubmission(_ values: NSSet)
+
+    @objc(removeSubmission:)
+    @NSManaged public func removeFromSubmission(_ values: NSSet)
+
+}
+
+extension SubmissionCalendarMO : Identifiable {
+
+    
+    func toDomain() -> SubmissionCalendarVO? {
+        guard let submissionSet = self.submission else { return nil }
+        
+        let _submissions =
+        submissionSet
+            .toArray(of: SubmissionMO.self)
+            .compactMap { Submission(managedContext: $0) }
+        
+        return SubmissionCalendarVO(dates: _submissions.compactMap(\.createdAt))
+    }
+}
+
+extension SubmissionCalendarVO {
+    func toMO() -> SubmissionCalendarMO? {
+        return nil
+    }
+}
