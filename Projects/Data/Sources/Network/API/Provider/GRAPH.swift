cf4a8cccae07782a526d62424730ad997b2ecf2a
pHH <gudghks56@gmail.com>
2023-12-14 19:23:09 +0900

    chore: API 로그 , 주석 , module import 코드 추가

--- Projects/Data/Sources/Network/API/Provider/GRAPH.swift
+++ Projects/Data/Sources/Network/API/Provider/GRAPH.swift
@@ -8,23 +8,67 @@
 
 import Foundation
 
+
 public enum GRAPH {
-    case PR_BY_ID(ID)                                              // 특정 ID의 문제
-    case PR_LIST(offset: Int, sort: String, order: String)         // 문제 리스트
-    /// dfasf
-    case SER_PR_LIST(keyword: String, limit: Int, order: String, sort: String)     // 문제 검색 리스트
-    case SUB_LIST(offset: Int, sort: String, order: String)        // 제출 리스트
-    case SUB_BY_PR_ID(offset: Int, problemId: Double)              // 제출 By 문제 아이디
-    case SUB_BY_ID(id: ID)                                         // 제출 By ID
-    case SUB_LIST_ME(username: String)                             // 나의 제출 리스트
     
-    case ME_SUB_HISTORY(limit: Int, offset: Int)    // 나의 제출 기록 (날짜와 아이디만)
-    case ME                                         // 멤버 조회
+    /// 특정 ID의 문제
+    case PR_BY_ID(ID)
+    /// 문제 리스트
+    case PR_LIST(arg: GRAR)
     
-    case SOLVE_PR_LIST(username: String)     // solve한 문제 리스트
-    case LANG_LIST(offset: Int)              // 언어 리스트
-    case TAG_LIST                            // Tag 리스트
+    /// 문제 검색 리스트
+    case SER_PR_LIST(keyword: String, arg: GRAR)
     
-    case UPDATE_NICKNAME(nickname: String)   // 닉네임 업데이트
-    case SUBMIT_SUB(languageId: ID, problemId: ID,sourceCode: String)  // 코드 제출하기
+    /// 제출 리스트
+    case SUB_LIST(arg: GRAR)
+    /// 제출 By 문제 아이디
+    case SUB_BY_PR_ID(offset: Int, problemId: Double)
+    /// 제출 By ID
+    case SUB_BY_ID(id: ID)
+    /// 나의 제출 리스트/
+    case SUB_LIST_ME(username: String)
+    /// 나의 제출 기록 (날짜와 아이디만)
+    case ME_SUB_HISTORY(limit: Int, offset: Int)
+    /// 멤버 조회
+    case ME
+    
+    /// solve한 문제 리스트
+    case SOLVE_PR_LIST(username: String)
+    /// 언어 리스트/
+    case LANG_LIST(offset: Int)
+    /// Tag 리스트
+    case TAG_LIST
+    /// 닉네임 업데이트/
+    case UPDATE_NICKNAME(nickname: String)
+    /// 코드 제출하기/
+    case SUBMIT_SUB(submit: GRSubmit)
+}
+
+
+/// Graph Argument
+public struct GRAR {
+    public let limit: Int
+    public let offset: Int
+    public let sort: String
+    public let order: String
+    
+    public init(limit: Int = 20, offset: Int, sort: String = "id", order: String = "asc") {
+        self.limit = limit
+        self.offset = offset
+        self.sort = sort
+        self.order = order
+    }
+}
+
+/// Graph Argument
+public struct GRSubmit {
+    public let languageId: ID
+    public let problemId: ID
+    public let sourceCode: String
+    
+    public init(languageId: ID, problemId: ID, sourceCode: String) {
+        self.languageId = languageId
+        self.problemId = problemId
+        self.sourceCode = sourceCode
+    }
 }
