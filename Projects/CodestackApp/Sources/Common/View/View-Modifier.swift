8b04284a5839047aad7bb9ed249107dacd281a35
pHH <gudghks56@gmail.com>
2024-01-15 23:49:18 +0900

    feat: SwiftUI Modifier 웹뷰 height 읽기 modifier 추가

---
+++ Projects/CodestackApp/Sources/Common/View/View-Modifier.swift
@@ -0,0 +1,32 @@
+//
+//  View-Modifier.swift
+//  CodestackApp
+//
+//  Created by 박형환 on 1/9/24.
+//  Copyright © 2024 hyeong. All rights reserved.
+//
+
+import SwiftUI
+
+extension View {
+    @ViewBuilder
+    func onReadSize(_ perform: @escaping (CGSize) -> Void) -> some View {
+        self.customBackground {
+            GeometryReader { geometryProxy in
+                Color.clear
+                    .preference(key: SizePreferenceKey.self, value: geometryProxy.size)
+            }
+        }
+        .onPreferenceChange(SizePreferenceKey.self, perform: perform)
+    }
+    
+    @ViewBuilder
+    func customBackground<V: View>(alignment: Alignment = .center, @ViewBuilder content: () -> V) -> some View {
+        self.background(alignment: alignment, content: content)
+    }
+}
+
+struct SizePreferenceKey: PreferenceKey {
+    static var defaultValue: CGSize = .zero
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) { }
+}
