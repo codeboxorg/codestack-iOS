4d0756ad10827f92597455ec82679da66ca1b315
pHH <gudghks56@gmail.com>
2024-01-16 01:02:52 +0900

    feat: SwiftUI Toast call Function add

--- Projects/CodestackApp/Sources/Common/View/Toast.swift
+++ Projects/CodestackApp/Sources/Common/View/Toast.swift
@@ -6,8 +6,76 @@
 //
 
 import UIKit
+import SwiftUI
 
-struct Toast{
+struct Toast {
+    static func toastMessage(_ value: ToastValue,
+                             _ pos: Position.Y,
+                             _ xOffset: CGFloat = 12,
+                             _ yOffset: CGFloat = 140,
+                             _ height: CGFloat = 50,
+                             container: UIView? = nil) {
+        
+        let moveUp = Animation.moveUpElement
+        let mmoveDown = Animation.moveDownElement
+        let toastV2 = ToastMessageViewV2(value: ToastValue.sample) { binding in
+            binding.wrappedValue.toggle()
+        }
+        
+        let messageView = UIHostingController(rootView: toastV2).view!
+        messageView.backgroundColor = .clear
+        
+        let width = Position.screenWidth - (xOffset * 2)
+        let height: CGFloat = height
+        
+        let yPos = Position.getYpos(pos) - yOffset
+        
+        let rect = Position.getPosition(xPosition: xOffset,
+                                        yPosition: yPos,
+                                        yOffset: 0,
+                                        defaultWidth: width,
+                                        defaultHeight: height)
+        
+        let afterPos: CGPoint = rect.origin
+        let initPos: CGPoint = .init(x: afterPos.x, y: Position.screenHeihgt)
+        messageView.frame = CGRect(origin: initPos, size: rect.size)
+        
+        messageView.alpha = 0.3
+        
+        if let container {
+            container.addSubview(messageView)
+        } else {
+            UIApplication.shared.keyWindow?.addSubview(messageView)
+        }
+        
+        UIView.animate(
+            withDuration: moveUp.duration,
+            delay: moveUp.delay,
+            options: moveUp.options,
+            animations: {
+                messageView.frame.origin = afterPos
+                messageView.alpha = moveUp.alpha
+            }
+            ,completion: {_ in
+                messageView.frame.origin = afterPos
+                DispatchQueue.main.asyncAfter(deadline: .now() + mmoveDown.delay) {
+                    UIView.animate(
+                        withDuration: mmoveDown.duration,
+                        delay: 0,
+                        options: mmoveDown.options,
+                        animations: {
+                            messageView.frame.origin = initPos
+                            messageView.alpha = mmoveDown.alpha
+                        }
+                        ,completion: {_ in
+                            messageView.removeFromSuperview()
+                        })
+                }
+            })
+    }
+    
+    
+    
     static func toastMessage(_ message: String,
                              container: UIView? = nil,
                              x position: CGFloat = 25 ,
@@ -36,28 +104,26 @@ struct Toast{
             UIApplication.shared.keyWindow?.addSubview(messageView)
         }
         
-        
         UIView.animate(
-             withDuration: appearElement.duration,
-             delay: appearElement.delay,
-             options: appearElement.options,
-             animations: {
-                 messageView.transform = appearElement.scale
-                 messageView.alpha = appearElement.alpha
-             }
-             ,completion: {_ in
-                 
-                 UIView.animate(
-                      withDuration: disappearElement.duration,
-                      delay: disappearElement.delay,
-                      options: disappearElement.options,
-                      animations: {
-                          messageView.transform = disappearElement.scale
-                          messageView.alpha = disappearElement.alpha
-                      }
-                      ,completion: {_ in
-                          messageView.removeFromSuperview()
-                      })
-             })
-         }
+            withDuration: appearElement.duration,
+            delay: appearElement.delay,
+            options: appearElement.options,
+            animations: {
+                messageView.transform = appearElement.scale
+                messageView.alpha = appearElement.alpha
+            }
+            ,completion: {_ in
+                UIView.animate(
+                    withDuration: disappearElement.duration,
+                    delay: disappearElement.delay,
+                    options: disappearElement.options,
+                    animations: {
+                        messageView.transform = disappearElement.scale
+                        messageView.alpha = disappearElement.alpha
+                    }
+                    ,completion: {_ in
+                        messageView.removeFromSuperview()
+                    })
+            })
+    }
 }
