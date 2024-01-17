183e71b252235cef3427e6a6ecc0b6b526c9f1fb
pHH <gudghks56@gmail.com>
2024-01-17 23:12:40 +0900

    move: 경로 그룹 네이밍 interface로 변경

---
+++ Projects/Module/CSNetwork/Sources/Interface/Endpoint.swift
@@ -0,0 +1,68 @@
+//
+//  Endpoint.swift
+//  Network
+//
+//  Created by 박형환 on 1/17/24.
+
+//  Copyright © 2024 hyeong. All rights reserved.
+//
+
+import Foundation
+
+public protocol EndPoint {
+    var host: String { get }
+    var scheme: String { get }
+    var path: String { get }
+    var method: RequestMethod { get }
+    var header: [String: String] { get set }
+    var body: Data? { get set }
+    var queryParams: [String: String]? { get } // Added for query parameters
+}
+
+public extension EndPoint {
+    
+    var scheme: String {
+        return "https"
+    }
+    var host: String {
+        return ""
+    }
+    
+    @discardableResult
+    mutating func setToken(_ token: String) -> Self {
+        self.header["Authorization"] = "Bearer \(token)"
+        return self
+    }
+    
+    @discardableResult
+    mutating func setBody(_ body: Data) -> Self {
+        self.body = body
+        return self
+    }
+    
+    func createRequest() throws -> URLRequest {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = self.scheme
+        urlComponents.host = self.host
+        urlComponents.path = self.path
+        urlComponents.queryItems = []
+        
+        self.queryParams?.forEach { key, value in
+            let item = URLQueryItem(name: key, value: value)
+            urlComponents.queryItems?.append(item)
+        }
+        
+        guard let url = urlComponents.url else {
+            throw NetworkError.badURLError
+        }
+        
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = self.method.rv
+        request.allHTTPHeaderFields = self.header
+        if let body = self.body {
+            request.httpBody = body
+        }
+        return request
+    }
+}
