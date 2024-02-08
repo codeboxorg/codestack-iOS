e46b31cbe210a4233bdb53c5af77e643028761c7
pHH <gudghks56@gmail.com>
2024-02-08 13:11:30 +0900

    feat/home: HomeView Compositional Layout extension 추가

--- Projects/CodestackApp/Sources/Presentation/Main/interface/CellIdentifierProtocol.swift
+++ Projects/CodestackApp/Sources/Presentation/Main/interface/CellIdentifierProtocol.swift
@@ -16,36 +16,36 @@ extension CellIdentifierProtocol{
     }
 }
 
-extension RecentSectionHeader: CellIdentifierProtocol{}
-extension HistoryCell: CellIdentifierProtocol{ }
-extension ProblemCell: CellIdentifierProtocol{ }
-extension SubmissionCell: CellIdentifierProtocol{}
-extension PRSubmissionTableViewCell: CellIdentifierProtocol {}
-extension OnBoardingCell: CellIdentifierProtocol {}
-extension OnBoardingHeader : CellIdentifierProtocol {}
+extension RecentSectionHeader: CellIdentifierProtocol { }
+extension HistoryCell: CellIdentifierProtocol { }
+extension ProblemCell: CellIdentifierProtocol { }
+extension SubmissionCell: CellIdentifierProtocol{ }
+extension OnBoardingCell: CellIdentifierProtocol { }
+extension OnBoardingHeader : CellIdentifierProtocol { }
+extension WritingListCell: CellIdentifierProtocol { }
 
-
-extension PRSubmissionHistoryCell: CellIdentifierProtocol{
+extension PRSubmissionHistoryCell: CellIdentifierProtocol {
     static func submissionHistoryCellSetting(item size: CGSize = CGSize(),
                                       spacing: CGFloat = 0,
                                       insetValue: CGFloat = 0,
                                              background color: UIColor? = .tertiarySystemBackground)
     -> UICollectionView
     {
-        
         let compositional = UICollectionView.getCompositional()
         
         compositional.configuration.scrollDirection = .vertical
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: compositional)
         
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: compositional)
         collectionView.register(RecentSectionHeader.self,
                                 forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: RecentSectionHeader.identifier)
         collectionView.register(PRSubmissionHistoryCell.self, forCellWithReuseIdentifier: PRSubmissionHistoryCell.identifier)
+        collectionView.register(WritingListCell.self, forCellWithReuseIdentifier: WritingListCell.identifier)
         
         collectionView.showsHorizontalScrollIndicator = false
         collectionView.backgroundColor = color
+        collectionView.isScrollEnabled = false
         
-        // 왜 contentInset 설정하면 layout scrollDirection이 수평, 수직 다 적용된다.
+        // contentInset 설정하면 layout scrollDirection이 수평, 수직 다 적용된다.
         collectionView.contentInset = UIEdgeInsets(top: insetValue,
                                                    left: insetValue,
                                                    bottom: insetValue,
@@ -79,41 +79,85 @@ enum SectionKind: Int, CaseIterable {
 extension UICollectionView{
     static func getCompositional() -> UICollectionViewCompositionalLayout {
         let config = UICollectionViewCompositionalLayoutConfiguration()
-        config.interSectionSpacing = 20
+        config.interSectionSpacing = 10
         config.scrollDirection = .vertical
         
         let layout =
-        UICollectionViewCompositionalLayout(sectionProvider:{
+        UICollectionViewCompositionalLayout(sectionProvider: {
             (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
-            // ✅ leadingItem의 contentInsets을 할당
-            let leadingItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(
-                widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0)))
-            leadingItem.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
-            
-            // ✅ group의 fractionalWidth를 1.0 보다 작게 설정
-            let containerGroup = NSCollectionLayoutGroup.horizontal(
-                layoutSize: NSCollectionLayoutSize(widthDimension:.fractionalWidth(0.5),
-                                                   heightDimension: .fractionalHeight(0.8)),
-                subitems: [leadingItem])  // 주의 - trailingItem이 아니라 trailingGroup
-            
-            
-            let section = NSCollectionLayoutSection(group: containerGroup)
-            
-            // ✅ orthogonalScrollingBehavior 설정
-            section.orthogonalScrollingBehavior = .continuous
-            
-            // section header 설정
-            let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
-                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                   heightDimension: .estimated(20)),
-                elementKind: UICollectionView.elementKindSectionHeader,
-                alignment: .top)
-            
-            
-            section.boundarySupplementaryItems = [sectionHeader]
-            return section
-
+            if sectionIndex == 0 {
+                return UICollectionView.firstSectionItem()
+            }
+            if sectionIndex == 1 {
+                return UICollectionView.secondSectionItem()
+            }
+            return UICollectionView.firstSectionItem()
         }, configuration: config)
         return layout
+    }
+    
+    static func firstSectionItem() -> NSCollectionLayoutSection {
+        // ✅ leadingItem의 contentInsets을 할당
+        
+        let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                          heightDimension: .fractionalHeight(0.9))
+        let leadingItem = NSCollectionLayoutItem(layoutSize: size)
+        
+        leadingItem.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+        
+        // ✅ group의 fractionalWidth를 1.0 보다 작게 설정
+        let containerGroup = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(widthDimension:.fractionalWidth(0.55),
+                                               heightDimension: .absolute(150)),
+            subitems: [leadingItem]
+        )  
+        // 주의 - trailingItem이 아니라 trailingGroup
+        let section = NSCollectionLayoutSection(group: containerGroup)
+        
+        // ✅ orthogonalScrollingBehavior 설정
+        section.orthogonalScrollingBehavior = .groupPaging
+        
+        // section header 설정
+        let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .estimated(20)),
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .top)
+        
+        section.contentInsets = .init(top: 10, leading: 0, bottom: 0, trailing: 0)
+        
+        section.boundarySupplementaryItems = [sectionHeader]
+        return section
+    }
+    
+    static func secondSectionItem() -> NSCollectionLayoutSection {
+        let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                          heightDimension: .fractionalHeight(1.0))
+        let leadingItem = NSCollectionLayoutItem(layoutSize: size)
+        
+        leadingItem.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension:.fractionalWidth(0.8),
+                                               heightDimension: .absolute(220))
+        let containerGroup = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitems: [leadingItem]
+        )
+        
+        let section = NSCollectionLayoutSection(group: containerGroup)
+        
+        section.orthogonalScrollingBehavior = .groupPaging
+        
+        // section header 설정
+        let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .estimated(20)),
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .top)
+        
+        section.contentInsets = .init(top: 10, leading: 0, bottom: 0, trailing: 0)
+        
+        section.boundarySupplementaryItems = [sectionHeader]
+        return section
     }
 }
