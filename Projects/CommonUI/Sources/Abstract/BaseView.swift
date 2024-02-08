21bbf0cea52f718f3b755607918c076b8ee1d339
pHH <gudghks56@gmail.com>
2024-02-08 13:19:09 +0900

    feat/CommonUI: 재사용을 위한 Abstrct Base Vc, View, Button 추가

---
+++ Projects/CommonUI/Sources/Abstract/BaseView.swift
@@ -0,0 +1,33 @@
+//
+//  BaseView.swift
+//  CommonUI
+//
+//  Created by 박형환 on 2/1/24.
+//  Copyright © 2024 hyeong. All rights reserved.
+//
+
+import UIKit
+
+open class BaseView: UIView {
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        addAutoLayout()
+        applyAttributes()
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("required init fatalError")
+    }
+    
+    public override func draw(_ layer: CALayer, in ctx: CGContext) {
+        super.draw(layer, in: ctx)
+        layerAttributes()
+    }
+    
+    open func layerAttributes() { }
+    
+    open func addAutoLayout() { }
+    
+    open func applyAttributes() { }
+}
---
+++ Projects/CommonUI/Sources/Abstract/BaseViewController.swift
@@ -0,0 +1,38 @@
+//
+//  BaseViewController.swift
+//  CommonUI
+//
+//  Created by 박형환 on 2/1/24.
+//  Copyright © 2024 hyeong. All rights reserved.
+//
+
+import UIKit
+
+open class BaseViewController<ContentView>: UIViewController {
+    typealias ContentView = UIView
+    
+    public init(contentView: ContentView) {
+        self.contentView = contentView
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    open var contentView: ContentView
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        addAutoLayout()
+        applyAttributes()
+        binding()
+    }
+    
+    open func addAutoLayout() { }
+    
+    open func applyAttributes() { }
+    
+    open func binding() { }
+}
+
