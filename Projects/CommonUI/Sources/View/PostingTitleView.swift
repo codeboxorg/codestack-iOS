d67467b8277471f996d20888bd530b33f70e0992
pHH <gudghks56@gmail.com>
2024-02-08 13:24:39 +0900

    feat/CommonUI: Common UI 추가, TitleView, StepView

---
+++ Projects/CommonUI/Sources/View/PostingTitleView.swift
@@ -0,0 +1,94 @@
+//
+//  PostingTitleView.swift
+//  CommonUI
+//
+//  Created by 박형환 on 2/1/24.
+//  Copyright © 2024 hyeong. All rights reserved.
+//
+
+import UIKit
+
+
+public struct WriterInfo {
+    public let title: String
+    public let writer: String
+    public let date: String
+    public let image: UIImage
+    
+    public init(title: String, writer: String, date: String, image: UIImage) {
+        self.title = title
+        self.writer = writer
+        self.date = date
+        self.image = image
+    }
+    
+    public static var mock: Self {
+        .init(title: "TestTestTestTestTestTestTestTestTestTest",
+              writer: "Test",
+              date: "2012-12-12",
+              image: UIImage(systemName: "circle") ?? UIImage())
+    }
+}
+
+public final class PostingTitleView: BaseView {
+    
+    private let titleLabel = UILabel()
+    private let writerLabel = UILabel()
+    private let dateLabel = UILabel()
+    private let profileImageView = UIImageView()
+    
+    public override func applyAttributes() {
+        let descriptor = titleLabel.create_descriptor(style: .title1)
+        titleLabel.font = UIFont(descriptor: descriptor, size: 28)
+        writerLabel.font = UIFont.boldSystemFont(ofSize: 13)
+        dateLabel.font = UIFont.boldSystemFont(ofSize: 13)
+        
+        titleLabel.textColor = .label
+        writerLabel.textColor = .label
+        dateLabel.textColor = .lightGray
+        
+        titleLabel.numberOfLines = 0
+        profileImageView.contentMode = .scaleAspectFill
+        profileImageView.clipsToBounds = true
+        profileImageView.layer.cornerRadius = 25
+        profileImageView.layer.borderWidth = 1
+        profileImageView.layer.borderColor = UIColor.sky_blue.cgColor
+    }
+    
+    public func apply(_ viewmodel: WriterInfo) {
+        self.titleLabel.text = viewmodel.title
+        self.writerLabel.text = viewmodel.writer
+        self.dateLabel.text = viewmodel.date
+        self.profileImageView.image = viewmodel.image
+    }
+    
+    public override func addAutoLayout() {
+        [
+            titleLabel,
+            writerLabel,
+            dateLabel,
+            profileImageView
+        ].forEach {
+            addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+
+            profileImageView.widthAnchor.constraint(equalToConstant: 50),
+            profileImageView.heightAnchor.constraint(equalToConstant: 50),
+            profileImageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 12),
+            profileImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            profileImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -12),
+            
+            writerLabel.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 12),
+            writerLabel.bottomAnchor.constraint(equalTo: profileImageView.centerYAnchor, constant: -4),
+            
+            dateLabel.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 12),
+            dateLabel.topAnchor.constraint(equalTo: profileImageView.centerYAnchor, constant: 4),
+        ])
+    }
+}
