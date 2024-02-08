34bf067697696b8c8bc7df3991250f0a26216df1
pHH <gudghks56@gmail.com>
2024-02-08 12:56:09 +0900

    feat: SubmissionResultView View 코드 정리

---
+++ Projects/CommonUI/Sources/Button/NumberButton.swift
@@ -0,0 +1,92 @@
+//
+//  NumberButton.swift
+//  CodeStack
+//
+//  Created by 박형환 on 2023/08/07.
+//
+
+import UIKit
+import SnapKit
+
+public final class NumberButton: UIButton {
+    
+    public var container: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private let numberLayerContainer: UIView = {
+        let number = UIView()
+        number.layer.borderColor = UIColor.sky_blue.cgColor
+        number.layer.borderWidth = 1
+        number.layer.cornerRadius = 5
+        return number
+    }()
+    
+    private let numberLabel: UILabel = {
+        let label = UILabel()
+        return label.headLineLabel(size: 16, text: "1", color: .label)
+    }()
+    
+    private let textlabel: UILabel = {
+        let label = UILabel()
+        return label.headLineLabel(size: 16, text: "결과", color: .label)
+    }()
+    
+    
+    public func setTextColor(color: UIColor) {
+        numberLabel.textColor = color
+        textlabel.textColor = color
+    }
+    
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        layoutConfigure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("required init fatalError")
+        
+    }
+    
+    public func settingNumber(for index: Int){
+        numberLabel.text = "\(index)"
+    }
+    
+    public func settingText(for text: String){
+        textlabel.text = text
+    }
+    
+    private func layoutConfigure(){
+        
+        addSubview(container)
+        
+        [numberLayerContainer,
+         textlabel]
+            .forEach{
+                container.addSubview($0)
+        }
+        
+        numberLayerContainer.addSubview(numberLabel)
+        
+        container.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        numberLayerContainer.snp.makeConstraints { make in
+            make.top.leading.bottom.equalToSuperview().inset(5)
+            make.width.height.equalTo(30)
+        }
+        
+        numberLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        textlabel.snp.makeConstraints { make in
+            make.leading.equalTo(numberLayerContainer.snp.trailing).offset(5)
+            make.centerY.equalTo(numberLayerContainer.snp.centerY)
+            make.trailing.equalToSuperview().inset(5)
+        }
+    }
+}
