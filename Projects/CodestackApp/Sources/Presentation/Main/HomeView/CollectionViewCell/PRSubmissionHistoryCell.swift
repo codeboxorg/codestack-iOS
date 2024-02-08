e429f202113bea38e36fc88e49d5c3a612eab37e
pHH <gudghks56@gmail.com>
2024-02-08 13:08:21 +0900

    refactor/home : 홈뷰 코드 정리

--- Projects/CodestackApp/Sources/Presentation/Main/HomeView/CollectionViewCell/PRSubmissionHistoryCell.swift
+++ Projects/CodestackApp/Sources/Presentation/Main/HomeView/CollectionViewCell/PRSubmissionHistoryCell.swift
@@ -12,21 +12,22 @@ import RxCocoa
 import RxSwift
 import Domain
 import Global
+import CommonUI
 
 final class PRSubmissionHistoryCell: UICollectionViewCell {
 
     
-    private let titleLabel: UILabel = {
+    let titleLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .left
         label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.layer.cornerRadius = 6
         label.textColor = .label
         return label
     }()
     
-    private let contentImageView: UIImageView = {
+    let contentImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.image = UIImage(systemName: "list.clipboard.fill")
         imageView.contentMode = .scaleToFill
         imageView.tintColor = UIColor.sky_blue
         // LAYER 계층을 위로 올린다
@@ -34,54 +35,24 @@ final class PRSubmissionHistoryCell: UICollectionViewCell {
         return imageView
     }()
     
-    private let statusLabel: UILabel = {
+    let statusLabel: UILabel = {
         let label = UILabel()
         label.text = ""
         label.textAlignment = .left
+        label.layer.cornerRadius = 4
         return label
     }()
     
-    private lazy var dateLabel: UILabel = {
+    lazy var dateLabel: UILabel = {
         let label = UILabel()
+        label.layer.cornerRadius = 4
         return label.descriptionLabel(size: 13, text: "", color: .systemGray)
     }()
-    
-    
-    var onRecentPageData = PublishRelay<SubmissionVO>()
-    var onStatus = PublishRelay<SolveStatus>()
-    
-    var cellDisposeBag = DisposeBag()
-    var disposeBag = DisposeBag()
-    
-    
+
     override init(frame: CGRect) {
-        
         super.init(frame: frame)
-        
         addAutoLayout()
-        
-        onRecentPageData
-            .asDriver(onErrorJustReturn: .sample)
-            .drive(onNext: {[weak self] submission in
-                guard let self = self else {return}
-                self.titleLabel.text = submission.problem.title
-                
-                if let solvedDate = submission.createdAt.toDateStringKST(format: .FULL){
-                    let dateString = DateCalculator().caluculateTime(solvedDate)
-                    self.dateLabel.text = dateString
-                }
-                
-            }).disposed(by: cellDisposeBag)
-        
-        onStatus
-            .asDriver(onErrorJustReturn: .none)
-            .drive(onNext: { [weak self] status in
-                guard let self = self else {return}
-                self.statusLabel.pr_status_label(status)
-            }).disposed(by: cellDisposeBag)
     }
-    
-    
     
     override func draw(_ rect: CGRect) {
         super.draw(rect)
@@ -102,7 +73,6 @@ final class PRSubmissionHistoryCell: UICollectionViewCell {
     
     override func prepareForReuse() {
         super.prepareForReuse()
-        self.disposeBag = DisposeBag()
     }
     
     
@@ -121,12 +91,8 @@ final class PRSubmissionHistoryCell: UICollectionViewCell {
         self.contentView.addGestureRecognizer(pinch)
         
         self.backgroundColor = UIColor.clear
-        contentView.backgroundColor = UIColor.systemBackground
-        contentView.layer.borderWidth = 1
-        contentView.layer.borderColor = UIColor.label.cgColor
+        contentView.backgroundColor = dynamicBackground()
         contentView.layer.cornerRadius = 10
-        
-        
         
         self.contentView.addSubview(titleLabel)
         self.contentView.addSubview(statusLabel)
@@ -134,21 +100,21 @@ final class PRSubmissionHistoryCell: UICollectionViewCell {
         self.contentView.addSubview(dateLabel)
         
         statusLabel.snp.makeConstraints{
-            $0.top.equalToSuperview().offset(-4)
+            $0.top.equalToSuperview().offset(4)
             $0.leading.equalToSuperview().inset(16)
-            $0.trailing.equalToSuperview().inset(10)
+            $0.trailing.lessThanOrEqualToSuperview().inset(10)
         }
         
         titleLabel.snp.makeConstraints{
             $0.top.equalTo(statusLabel.snp.bottom).offset(6)
-            $0.leading.equalToSuperview().inset(10)
-            $0.trailing.equalToSuperview().inset(5)
+            $0.leading.equalToSuperview().inset(6)
+            $0.trailing.lessThanOrEqualToSuperview().inset(6)
         }
         
         contentImageView.snp.makeConstraints{
             $0.centerY.equalToSuperview().offset(25)
             $0.leading.equalToSuperview().inset(12)
-            $0.width.height.equalTo(50)
+            $0.width.height.equalTo(35)
         }
         
         dateLabel.snp.makeConstraints{
