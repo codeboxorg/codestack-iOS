d47b44bc4e81aa03f47edb4af2f5c65c41df6940
pHH <gudghks56@gmail.com>
2024-02-08 13:24:04 +0900

    feat/CommonUI: Dynamic Color Extension

---
+++ Projects/CommonUI/Sources/Style/ColorMode.swift
@@ -0,0 +1,37 @@
+//
+//  ColorMode.swift
+//  CommonUI
+//
+//  Created by 박형환 on 2/1/24.
+//  Copyright © 2024 hyeong. All rights reserved.
+//
+
+import UIKit
+
+
+extension UITraitEnvironment {
+    public func dynamicBackground() -> UIColor {
+        UIColor(dynamicProvider: { trait in
+            if trait.userInterfaceStyle == .dark {
+                return .tertiarySystemBackground
+            } else {
+                return .whiteGray
+            }
+        })
+    }
+    
+    public var dynamicSysBackground: UIColor {
+        UIColor(dynamicProvider: { trait in
+            if trait.userInterfaceStyle == .dark {
+                return .systemBackground
+            } else {
+                return .whiteGray
+            }
+        })
+    }
+}
+
+
+
+
+
