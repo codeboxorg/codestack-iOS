9fd69a334926feed93bd08e2db84b0fd6cf3d043
pHH <gudghks56@gmail.com>
2024-02-08 13:09:36 +0900

    feat/home: HomeViewModel - RxDataSource , Section 변경에 따른 Model 변경 및 리팩토링 , RecentPageModel 추가

--- Projects/CodestackApp/Sources/Presentation/Main/ViewModel/RecentPageModel.swift
+++ Projects/CodestackApp/Sources/Presentation/Main/ViewModel/RecentPageModel.swift
@@ -8,18 +8,6 @@
 import Foundation
 import RxDataSources
 import Domain
-//typealias RecentSubmissionSection = SectionModel<String, RecentSubmission>
-
-// MARK: 확인후 제거
-//extension Submission: IdentifiableType, Equatable{
-//    static func == (lhs: Submission, rhs: Submission) -> Bool {
-//        lhs.id == rhs.id
-//    }
-//    
-//    var identity: some Hashable{
-//        return self.id
-//    }
-//}
 
 extension SubmissionVO: IdentifiableType {
     public var identity: some Hashable {
@@ -27,19 +15,32 @@ extension SubmissionVO: IdentifiableType {
     }
 }
 
-extension Array where Element == SubmissionVO {
-    func toRecentModels(title: String = "최근 제출 10개") -> [RecentSubmission] {
-        return [RecentSubmission(headerTitle: title, items: self)]
+extension Array where Element == StoreVO {
+    func toHomePostingSectionModel() -> HomeSection.HomeSectionModel {
+        let items = self.map { store in HomeSection.HomeItem.writingList(store) }
+        return HomeSection.HomeSectionModel(model: .writing, items: items)
     }
 }
 
-struct RecentSubmission {
+extension Array where Element == SubmissionVO {
+    func toRecentModels(title: String = "최근 제출 10개") -> [RecentSubmission] {
+        let items = self.map { subVo in RecentSubmission.Item.recent(subVo) }
+        return [RecentSubmission(headerTitle: title, items: items)]
+    }
+    
+    func toHomeSubmissionSectionModel() -> HomeSection.HomeSectionModel {
+        let items = self.map { subVo in HomeSection.HomeItem.recent(subVo) }
+        return HomeSection.HomeSectionModel(model: .recent, items: items)
+    }
+}
+
+struct RecentSubmission: Equatable {
     var headerTitle: String
     var items: [Item]
 }
 
 extension RecentSubmission: AnimatableSectionModelType {
-    typealias Item = SubmissionVO
+    typealias Item = HomeSection.HomeItem
     
     var identity: String {
         return headerTitle
@@ -48,5 +49,32 @@ extension RecentSubmission: AnimatableSectionModelType {
     init(original: RecentSubmission, items: [Item]) {
         self = original
         self.items = items
+    }
+}
+
+
+extension StoreVO: IdentifiableType {
+    public var identity: some Hashable {
+        return self.id
+    }
+}
+
+struct HomeSection {
+    typealias HomeSectionModel = SectionModel<HomeViewSection, HomeItem>
+    
+    enum HomeViewSection: String, Equatable {
+        case recent = "최근 제출 10개"
+        case writing = "관련 글"
+    }
+    
+    enum HomeItem: Equatable {
+        case recent(SubmissionVO)
+        case writingList(StoreVO)
+    }
+}
+
+extension HomeSection.HomeItem: IdentifiableType {
+    public var identity: some Hashable {
+        return UUID().uuidString
     }
 }
