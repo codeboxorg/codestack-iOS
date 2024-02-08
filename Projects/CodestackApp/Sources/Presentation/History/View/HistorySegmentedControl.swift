51e1430403a2c0283f2bdb22ab7fa83ffef1afae
pHH <gudghks56@gmail.com>
2024-02-08 13:02:34 +0900

    refactor: HistoryViewController 바인딩 로직, View SkeletonView 적용

--- Projects/CodestackApp/Sources/Presentation/History/View/HistorySegmentedControl.swift
+++ Projects/CodestackApp/Sources/Presentation/History/View/HistorySegmentedControl.swift
@@ -6,10 +6,9 @@
 //
 
 import UIKit
+import Global
 
-
-
-class HistorySegmentedControl: UISegmentedControl{
+class HistorySegmentedControl: UISegmentedControl {
     
     private let font = UIFont.boldSystemFont(ofSize: 14)
     
@@ -30,23 +29,24 @@ class HistorySegmentedControl: UISegmentedControl{
         }
     }
     
-    override func draw(_ rect: CGRect){
+    override func draw(_ layer: CALayer, in ctx: CGContext) {
+        super.draw(layer, in: ctx)
         let x = calculateWidth(for: selectedSegmentIndex)
-        let yHeight = self.bounds.size.height - 1.0
-        
-        segmentSelecteLayer(x: x, y: yHeight)
-        
+        let yHeight = layer.bounds.size.height - 1.0
+        self.layer.delegate = self
+
+        segmentSelecteLayer(x: x, y: yHeight, ctx: ctx)
         let separtorPointOne = calculateWidth(for: 2)
         let separtorPointTwo = calculateWidth(for: 1)
-        addSeparatorLine(x: separtorPointOne, y: rect.origin.y, height: yHeight)
-        addSeparatorLine(x: separtorPointTwo, y: rect.origin.y, height: yHeight)
+        addSeparatorLine(x: separtorPointOne, y: layer.bounds.origin.y, height: yHeight, ctx: ctx)
+        addSeparatorLine(x: separtorPointTwo, y: layer.bounds.origin.y, height: yHeight, ctx: ctx)
     }
     
     required init?(coder: NSCoder) {
         fatalError("required init fatalError")
     }
     
-    private func segmentSelecteLayer(x: CGFloat,y: CGFloat){
+    private func segmentSelecteLayer(x: CGFloat,y: CGFloat, ctx: CGContext) {
         // 첫번째 인덱스에서는 빈 배열로 값이 반환되기때문에 spacing 10을 더해준다.
         let xPosition = x + 10
         let yPosition = y
@@ -54,33 +54,25 @@ class HistorySegmentedControl: UISegmentedControl{
         /// String width with font
         let selectedSegWidth = segWidths[safe: selectedSegmentIndex] ?? 10
         
-        let layer = UIBezierPath()
-        
-        layer.lineWidth = 5.0
-        layer.move(to: CGPoint(x: xPosition, y: yPosition))
-        layer.addLine(to: CGPoint(x: xPosition + selectedSegWidth , y: yPosition))
-        UIColor.sky_blue.setStroke()
-        layer.stroke()
+        ctx.setLineWidth(5)
+        ctx.move(to: CGPoint(x: xPosition, y: yPosition))
+        ctx.addLine(to: CGPoint(x: xPosition + selectedSegWidth, y: yPosition))
+        ctx.setStrokeColor(UIColor.sky_blue.cgColor)
+        ctx.strokePath()
     }
     
-    
-    private func addSeparatorLine(x: CGFloat, y: CGFloat, height : CGFloat) {
+    private func addSeparatorLine(x: CGFloat, y: CGFloat, height : CGFloat, ctx: CGContext) {
         let x = x
         let y = y
         
         let separatorHeight = "Temp".height(withConstrainedWidth: .infinity, font: font)
         let spacing = (height - separatorHeight) / 2
         
-        let separatorPath = UIBezierPath()
-        separatorPath.lineWidth = 2.0
-        separatorPath.move(to: CGPoint(x: x,
-                                       y: y + spacing))
-        
-        separatorPath.addLine(to: CGPoint(x: x,
-                                          y: height - spacing))
-        
-        UIColor.lightGray.setStroke()
-        separatorPath.stroke()
+        ctx.setLineWidth(2)
+        ctx.move(to: CGPoint.init(x: x, y: y + spacing))
+        ctx.addLine(to: CGPoint.init(x: x, y: height - spacing))
+        ctx.setStrokeColor(UIColor.lightGray.cgColor)
+        ctx.strokePath()
     }
     
     
