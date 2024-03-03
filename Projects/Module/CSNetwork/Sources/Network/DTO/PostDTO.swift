6efa953f98625d8608582dfb0d750ea5e412c617
pHH <gudghks56@gmail.com>
2024-03-03 23:32:31 +0900

    fix: Post Endpoint Encoding 수정

--- Projects/Module/CSNetwork/Sources/Network/DTO/PostDTO.swift
+++ Projects/Module/CSNetwork/Sources/Network/DTO/PostDTO.swift
@@ -26,8 +26,9 @@ public struct Post: Codable {
     }
     
     public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: FieldKeys.self)
-        try container.encode(StringValue(value: markdown), forKey: .markdown)
+        var container = encoder.container(keyedBy: StoreKey.self)
+        var fieldContainer = container.nestedContainer(keyedBy: FieldKeys.self, forKey: .fields)
+        try fieldContainer.encode(StringValue(value: markdown), forKey: .markdown)
     }
     
     public init(from decoder: Decoder) throws {
