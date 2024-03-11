59a028a27e8067ee0e09308292487e3754f01845
pHH <gudghks56@gmail.com>
2024-03-12 05:40:48 +0900

    feat: Submission Array 날짜 관련 유틸 Extension 추가

--- Projects/Domain/Sources/Domain/Extension/Array+Extension.swift
+++ Projects/Domain/Sources/Domain/Extension/Array+Extension.swift
@@ -7,19 +7,46 @@
 //
 
 import Foundation
+import Global
 
-extension Array where Element == SubmissionVO {
+public extension Array where Element == SubmissionVO {
     
     /// 같은 Problem ID를 찾는 함수,
     /// 임시저장을 배열에서 일치하는 제출을 찾아서 업데이트 할때 필요한 extension 함수
     /// - Parameter model: SendProblemModel
     /// - Returns: 일치하는 Submission
     func find(model: SubmissionVO) -> SubmissionVO? {
-        return self.filter { value in
-            if value.problem.id == model.problem.id {
-                return true
-            }
-            return false
-        }.first
+        self.first(where: { $0.problem.id == model.problem.id ? true : false })
+    }
+    
+    func deleteEqual(problemID: ProblemID) -> Self {
+        self.filter { $0.problem.id == problemID ? false : true }
+    }
+    
+    func deleteEqualStatusCodes(receive: SubmissionVO) -> Self {
+        self.filter { subVO in
+            let con1 = subVO.problem.id == receive.problem.id
+            let con2 = subVO.statusCode == receive.statusCode
+            let con = con1 && con2
+            return con ? false : true
+        }
+    }
+    
+    func sortByDate(_ latest: Bool = true) -> Self {
+        self.sorted(by: { s1,s2 in
+            let createdAt1 = s1.createdAt.isKSTORUTC()
+            let createdAt2 = s2.createdAt.isKSTORUTC()
+            return latest ? createdAt1 > createdAt2 : createdAt1 < createdAt2
+        })
+    }
+}
+
+public extension Array where Element == StoreVO {
+    func sortByDate(_ latest: Bool = true) -> Self {
+        self.sorted(by: { s1,s2 in
+            let createdAt1 = s1.date.toYYMMDD() ?? Date()
+            let createdAt2 = s2.date.toYYMMDD() ?? Date()
+            return latest ? createdAt1 > createdAt2 : createdAt1 < createdAt2
+        })
     }
 }
