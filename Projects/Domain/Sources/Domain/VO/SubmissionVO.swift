bf53ec5fe9e419ec4d3fd8b7ad9dc89daaef6d9c
pHH <gudghks56@gmail.com>
2024-03-12 05:38:02 +0900

    feat: Domain ValueObject 추가 및 관련 Mock 데이터 추가

--- Projects/Domain/Sources/Domain/VO/SubmissionVO.swift
+++ Projects/Domain/Sources/Domain/VO/SubmissionVO.swift
@@ -29,13 +29,21 @@ public struct SubmissionVO: Codable, Equatable {
     public var problemVO: ProblemVO
     public var member: MemberNameVO
     public var language: LanguageVO
+    public var statusCode: SolveStatus
     public var cpuTime: Double
     public var memoryUsage: Double
-    public var statusCode: String
     public var createdAt: String
     
-    public init(id: String, sourceCode: String, problem: ProblemIdentityVO,
-                problemVO: ProblemVO = ProblemVO.sample ,member: MemberNameVO, language: LanguageVO, cpuTime: Double, memoryUsage: Double, statusCode: String, createdAt: String) {
+    public init(id: String,
+                sourceCode: String,
+                problem: ProblemIdentityVO,
+                problemVO: ProblemVO = ProblemVO.sample ,
+                member: MemberNameVO,
+                language: LanguageVO,
+                statusCode: SolveStatus,
+                cpuTime: Double,
+                memoryUsage: Double,
+                createdAt: String) {
         self.id = id
         self.sourceCode = sourceCode
         self.problem = problem
@@ -51,14 +59,51 @@ public struct SubmissionVO: Codable, Equatable {
 
 public extension SubmissionVO {
     static var sample: Self {
-        .init(id: "",
+        .init(id: "sampleUID1234",
               sourceCode: "",
-              problem: .init(id: "", title: ""),
+              problem: .init(id: "", title: "Sample"),
               member: .init(username: ""),
-              language: .init(id: "", name: "", extension: ""),
+              language: .init(id: "", name: "Swift", extension: ".swift"),
+              statusCode: .temp,
               cpuTime: -1,
               memoryUsage: -1,
-              statusCode: "",
-              createdAt: Date().toString())
+              createdAt: "1999-12-25 10:10:10")
+    }
+    
+    static var mock: Self {
+        .init(id: "TestUID1234",
+              sourceCode: "import Foudation",
+              problem: .init(id: "테스트 제출 모델", title: "Hello World 0"),
+              member: .init(username: "hwan"),
+              language: .init(id: "-1", name: "Swift", extension: ".swift"),
+              statusCode: .temp,
+              cpuTime: -1,
+              memoryUsage: -1,
+              createdAt: "2030-12-15 10:10:10")
+    }
+    
+    
+    /// Sample은 CollectionView TableView를 위해서 뒤에 추가
+    /// - Parameter count: mock 개수
+    /// - Returns: Mock list
+    static func getMocksForHome(_ count: Int = 3) -> [Self] {
+        (0...count).map { _ in SubmissionVO.mock }
+    }
+    
+    var isMock: Bool {
+        self.id == "TestUID1234"
+    }
+    var isNotMock: Bool {
+        self.id != "TestUID1234"
+    }
+    
+    var isSample: Bool {
+        self.id == "sampleUID1234"
+    }
+    
+    func toSubmitMut() -> SubmitMutation {
+        SubmitMutation(languageId: language.id,
+                       problemId: problem.id,
+                       sourceCode: sourceCode)
     }
 }
