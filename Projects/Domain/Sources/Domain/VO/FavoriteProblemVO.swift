23cdeb00c68a623bf28983e7a8eaab68802f0d50
pHH <gudghks56@gmail.com>
2024-03-12 05:38:02 +0900

    feat: Domain ValueObject 추가 및 관련 Mock 데이터 추가

--- Projects/Domain/Sources/Domain/VO/FavoriteProblemVO.swift
+++ Projects/Domain/Sources/Domain/VO/FavoriteProblemVO.swift
@@ -19,3 +19,25 @@ public struct FavoriteProblemVO {
         self.createdAt = createdAt
     }
 }
+
+public extension FavoriteProblemVO {
+    
+    func toSubmissionVO() -> SubmissionVO {
+        
+        // TODO: Favorite Model 변경 예정
+        // Favorite Problem VO 에는 submission ID값이 없음 왜냐하면 제출을 안한 상태이기 때문에
+        // 일단 UUID를 넣어주고, 추후에 생각? -> 지금은 HIstory VIew 에서만 Favorite를 보여준다고 하면
+        // favorite에서 들어갔을때 -> 기존에 임시저장을 하고 있었다면 -> fetch 해와야함 DBREpository 에서
+        // PRoblem의 Language List를 가져와야함 -> 지금은 Sample
+        
+        SubmissionVO(id: UUID().uuidString,
+                     sourceCode: "",
+                     problem: ProblemIdentityVO(id: problemID, title: problmeTitle),
+                     member: MemberVO.sample.toNameVO,
+                     language: .default,
+                     statusCode: .favorite,
+                     cpuTime: 0,
+                     memoryUsage: 0,
+                     createdAt: createdAt.toString())
+    }
+}
--- Projects/Domain/Sources/Domain/VO/FirebaseVO/DocumentVO.swift
+++ Projects/Domain/Sources/Domain/VO/FirebaseVO/DocumentVO.swift
@@ -20,28 +20,94 @@ public struct DocumentVO {
 
 public struct StoreVO: Equatable {
     public let id: String
+    public let userId: String
     public let title: String
-    public let name: String
+    public var name: String
     public let date: String
+    public var imageURL: String
     public let description: String
-    public let markdownID: String
+    public var markdownID: String
     public let tags: [String]
     
-    public init(title: String, name: String, date: String, description: String, markdown: String, tags: [String]) {
-        self.id = UUID().uuidString
+    public init(id: String = UUID().uuidString,
+                userId: String,
+                title: String,
+                name: String,
+                date: String,
+                imageURL: String = "", 
+                description: String,
+                markdown: String,
+                tags: [String]) {
+        self.id = id
         self.title = title
+        self.userId = userId
         self.name = name
         self.date = date
+        self.imageURL = imageURL
         self.description = description
         self.markdownID = markdown
         self.tags = tags
     }
 }
 
+public extension StoreVO {
+    static var `default`: Self {
+        .init(id: UUID().uuidString,
+              userId: "storm",
+              title: "",
+              name: "",
+              date: "",
+              description: "",
+              markdown: "",
+              tags: [])
+    }
+    static var mock: Self {
+        .init(id: "TestUID1234",
+              userId: "storm",
+              title: "StoreTest",
+              name: "TestStore",
+              date: "2030-12-15 10:10:10",
+              description: "Test Description Test Description Test Description Test Description",
+              markdown: "markdownIDString",
+              tags: ["구현", "알고리즘", "자료구조"])
+    }
+    
+    var isMock: Bool {
+        self.id == "TestUID1234"
+    }
+    
+    var isNotMock: Bool {
+        self.id != "TestUID1234"
+    }
+    
+    static func getMocksForHome(_ count: Int = 3) -> [Self] {
+        (0...count).map { _ in StoreVO.mock }
+    }
+    
+    static func makeViewModel(_ title: String, _ tags: [String]) -> StoreVO {
+        StoreVO(userId: "",
+                title: title,
+                name: "",
+                date: Date().toString(format: .YYYYMMDD),
+                description: "",
+                markdown: "",
+                tags: tags)
+    }
+    
+    func makeViewModel(nickname: String, imageURL: String) -> StoreVO {
+        StoreVO(userId: "",
+                title: self.title,
+                name: nickname,
+                date: self.date,
+                imageURL: imageURL,
+                description: self.description,
+                markdown: self.markdownID,
+                tags: tags)
+    }
+}
+
 public struct PostVO {
-    
     public let markdown: String
-    
     public init(markdown: String) {
         self.markdown = markdown
     }
