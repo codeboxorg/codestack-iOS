2def7f67991d1684b0e4550025e23a8abe69530e
pHH <gudghks56@gmail.com>
2024-03-12 05:40:48 +0900

    feat: Submission Array 날짜 관련 유틸 Extension 추가

--- Projects/Domain/Sources/Domain/Extension/SubmissionVO-Extension.swift
+++ Projects/Domain/Sources/Domain/Extension/SubmissionVO-Extension.swift
@@ -10,27 +10,14 @@ import Foundation
 
 
 public extension SubmissionVO {
+    
     /// 제출 같은 language와 source코드 여부
     /// - Parameter other: 다른 제출 모델
     /// - Returns: 같은 내용인지 flag
     func isEqualContent(other: Self) -> Bool {
-        if self.sourceCode == other.sourceCode,
-           self.language.id == other.language.id {
-            return true
-        } else {
-            return false
-        }
-    }
-}
-
-public extension Array where Element == SubmissionVO {
-    func sortByDate() -> Self {
-        self.sorted(by: { s1,s2 in
-            if let createdAt1 = s1.createdAt.toDateKST(),
-               let createdAt2 = s2.createdAt.toDateKST() {
-                return createdAt1 > createdAt2
-            }
-            return false
-        })
+        let con1 = self.sourceCode == other.sourceCode
+        let con2 = self.language.id == other.language.id
+        let condition = con1 && con2
+        return condition
     }
 }
