62a159831f9457679a0ab5a22966110b589ab586
pHH <gudghks56@gmail.com>
2024-04-01 13:53:23 +0900

    Feat: Auth 관련 Domain, Data class 추가

--- Projects/Domain/Sources/Interface/Parameters/RegisterQuery.swift
+++ Projects/Domain/Sources/Interface/Parameters/RegisterQuery.swift
@@ -10,15 +10,23 @@ import Foundation
 
 
 public struct RegisterQuery {
-    public let id: String
-    public let password: String
-    public let email: String
-    public let nickname: String
+    public var id: String
+    public var password: String
+    public var email: String
+    public var nickname: String
+    public var isCorrectPassWord: String
     
-    public init(id: String, password: String, email: String, nickname: String) {
+    public init(id: String = "", password: String = "", email: String = "", nickname: String = "", isCorrectPassWord: String = "") {
         self.id = id
         self.password = password
         self.email = email
         self.nickname = nickname
+        self.isCorrectPassWord = isCorrectPassWord
+    }
+}
+
+public extension RegisterQuery {
+    static var mock: Self {
+        .init(id: "", password: "", email: "", nickname: "")
     }
 }
