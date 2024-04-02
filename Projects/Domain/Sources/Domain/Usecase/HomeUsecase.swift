61ca7da046c92bf0b084c996b5ae508dda3736c0
pHH <gudghks56@gmail.com>
2024-04-02 10:17:09 +0900

    Feat: Home Usecase  의존성 , 및 함수 Problem fetch 함수 변경

--- Projects/Domain/Sources/Domain/Usecase/HomeUsecase.swift
+++ Projects/Domain/Sources/Domain/Usecase/HomeUsecase.swift
@@ -8,21 +8,34 @@
 
 import Foundation
 import RxSwift
-import Global
 
 public final class HomeUsecase {
     
+    private var disposebag = DisposeBag()
     private let webRepository: WebRepository
     private let dbRepository: DBRepository
+    private let submissionRepository: SubmissionRepository
     
-    public init(webRepository: WebRepository, dbRepository: DBRepository) {
+    public init(webRepository: WebRepository,
+                dbRepository: DBRepository,
+                submissionRepository: SubmissionRepository) {
         self.webRepository = webRepository
         self.dbRepository = dbRepository
+        self.submissionRepository = submissionRepository
     }
+    
+    #if DEBUG
+    public func removeForTest() {
+//        dbRepository.removeAll()
+//            .subscribe(onDisposed:  {
+//                Log.debug("completed")
+//            })
+    }
+    #endif
     
     public func fetchSubmissionList() -> Observable<[SubmissionVO]> {
         dbRepository
-            .fetchProblemState()
+            .fetchProblemState(.all)
             .map(\.first)
             .map { problemState in
                 if let problemState { return problemState.submissions.sortByDate() }
@@ -31,15 +44,27 @@ public final class HomeUsecase {
             .asObservable()
     }
     
-    public func fetchProblem(using subVO: SubmissionVO) -> Observable<SubmissionVO> {
-        webRepository
-            .getProblemByID(subVO.problem.id)
-            .map { /*prFR*/ prVo in
-                let problemVO = prVo /*.toDomain()*/
-                var submission = subVO
-                submission.problemVO = problemVO
-                return submission
-            }
+    public func fetchSubmissionEqual(_ code: SolveStatus) -> Observable<[SubmissionVO]> {
+        dbRepository.fetch(.isEqualStatusCode(code))
             .asObservable()
+            .map { $0 }
+    }
+    
+    public func fetchProblem(using subVO: SubmissionVO) -> Observable<SubmissionVO> {
+        dbRepository
+            .fetch(.is_Equal_ST_ID(subVO.id, subVO.statusCode))
+            .compactMap { $0.first }
+            .flatMap { [weak submissionRepository] vo in
+                guard let submissionRepository else { return .just(vo) }
+                return submissionRepository
+                    .fetchProblemByID(subVO.problem.id)
+                    .timeout(.milliseconds(400), scheduler: MainScheduler.instance)
+                    .map { prVO in
+                        var subVo = vo
+                        subVo.problemVO = prVO
+                        return subVO
+                    }
+                    .catchAndReturn(vo)
+            }.asObservable()
     }
 }
