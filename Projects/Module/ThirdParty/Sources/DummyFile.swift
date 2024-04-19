965c7489b10e65226f2754148723421adbf2eefe
pHH <gudghks56@gmail.com>
2024-04-20 08:17:29 +0900

    Tuist: 모듈 셋팅 변경

---
+++ Projects/Module/ThirdParty/Sources/DummyFile.swift
@@ -0,0 +1,28 @@
+//
+//  DummyFile.swift
+//  ThirdParty
+//
+//  Created by 박형환 on 1/18/24.
+//  Copyright © 2024 hyeong. All rights reserved.
+//
+
+import Foundation
+//@_exported import RxSwift
+//@_exported import RxCocoa
+//@_exported import Swinject
+//@_exported import RxFlow
+//@_exported import RxGesture
+//@_exported import Apollo
+//@_exported import CodestackAPI
+//@_exported import SnapKit
+//@_exported import Then
+//@_exported import RxDataSources
+//@_exported import RichText
+//@_exported import RichTextKit
+//@_exported import Highlightr
+//@_exported import FirebaseCore
+//
+//func linking_static_library() {
+//    /// 단순히 Linking 하기 위한 코드
+//    ///
+//}
