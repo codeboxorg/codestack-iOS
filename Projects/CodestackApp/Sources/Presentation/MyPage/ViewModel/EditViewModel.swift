d13774b2e00602c7b9651a8df902e16f142ad61e
pHH <gudghks56@gmail.com>
2024-04-28 09:27:59 +0900

    Feat: MyPage Profile Edit VC , Binder, Cache 적용 ,Viewmodel 추가

--- Projects/CodestackApp/Sources/Presentation/MyPage/ViewModel/EditViewModel.swift
+++ Projects/CodestackApp/Sources/Presentation/MyPage/ViewModel/EditViewModel.swift
@@ -9,14 +9,30 @@
 import Foundation
 import Domain
 
+enum UpdateState {
+    case imageState(Data,nickname: String)
+    case email(String)
+    case nickname(String)
+}
+
 struct EditViewModel {
     let imageURL: String
-    let image: Data
-    let email: String
-    let nickName: String
+    var image: Data
+    var email: String
+    var nickName: String
 }
 
 extension EditViewModel: Equatable {
+    
+    func isEqual(to vm: Self) -> [UpdateState] {
+        var state: [UpdateState] = []
+        if email != vm.email { state.append(.email(vm.email)) }
+        if nickName != vm.nickName { state.append(.nickname(vm.nickName)) }
+        if image != vm.image { state.append(.imageState(vm.image, nickname: nickName)) }
+        return state
+    }
+    
+    
     static var sample: Self {
         .init(imageURL: "codestack",
               image: Data(),
@@ -32,6 +48,7 @@ extension EditViewModel: Equatable {
                  profileImage: self.imageURL)
     }
 }
+
 extension MemberVO {
     func toVM(_ data: Data) -> EditViewModel {
         .init(imageURL: self.profileImage,
