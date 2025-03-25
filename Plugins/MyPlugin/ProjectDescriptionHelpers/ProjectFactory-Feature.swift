4d9653cb7ea6c1cff8fe92501e7c67f27d5bc589
pHH <gudghks56@gmail.com>
2025-03-25 14:50:55 +0900

    refactor: Tuist Plugin 으로 분리

---
+++ Plugins/MyPlugin/ProjectDescriptionHelpers/ProjectFactory-Feature.swift
@@ -0,0 +1,72 @@
+
+@preconcurrency import ProjectDescription
+
+
+
+public extension ProjectFactory {
+    static func createModuleWithDemo(
+        name: String,
+        product: Product,
+        platform: Platform = .iOS,
+        resources: ResourceFileElements? = ["Resources/**"],
+        entitlements: Entitlements? = nil,
+        dependencies: [TargetDependency] = [],
+        infoPlist: InfoPlist = .default
+    ) -> Project {
+        Project(
+            name: name,
+            organizationName: AppConfig.orgName,
+            options: .options(automaticSchemesOptions: .disabled),
+            settings: AppConfig.projectConfiguration(),
+            targets: [
+                .target(
+                    name: name,
+                    destinations: .iOS,
+                    product: product,
+                    bundleId: "\(AppConfig.orgName).\(name)",
+                    deploymentTargets: AppConfig.deploymentTarget,
+                    infoPlist: infoPlist,
+                    sources: ["Sources/**"],
+                    resources: resources,
+                    dependencies: dependencies,
+                    settings: AppConfig.targetConfiguration(path: name)
+                ),
+                
+                .target(
+                    name: "\(name)Demo",
+                    destinations: .iOS,
+                    product: .app,
+                    bundleId: "\(AppConfig.orgName).\(name)Demo",
+                    deploymentTargets: AppConfig.deploymentTarget,
+                    infoPlist: .extendingDefault(with: AppConfig.demoAppSceneDeleateSetting(name: name)),
+                    sources: ["Demo/**"],
+                    resources: ["Demo/Resources/**"],
+                    dependencies: [
+                        .target(name: name)
+                    ],
+                    settings: AppConfig.demoProjectConfiguration(path: name)
+                ),
+                
+                .target(
+                    name: "\(name)Tests",
+                    destinations: .iOS,
+                    product: .unitTests,
+                    bundleId: "\(AppConfig.orgName).\(name)Tests",
+                    deploymentTargets: AppConfig.deploymentTarget,
+                    infoPlist: .default,
+                    sources: ["Tests/**"],
+                    dependencies: [
+                        .target(name: name)
+                    ],
+                    settings: .settings(base: ["ENABLE_MODULE_VERIFIER": "YES"], configurations: [])
+                )
+            ],
+            schemes: [
+                .makeDemoScheme(name: name),
+                .makeScheme(config: .dev, name: name),
+                .makeScheme(config: .prod, name: name)
+            ],
+            additionalFiles: [.glob(pattern: .relativeToXCConfig(path: "shared"))]
+        )
+    }
+}
---
+++ Plugins/MyPlugin/ProjectDescriptionHelpers/Scheme.swift
@@ -0,0 +1,35 @@
+
+@preconcurrency import ProjectDescription
+
+// MARK: - Scheme 생성 확장
+extension Scheme {
+    static func makeScheme(config: BuildConfig, name: String) -> Scheme {
+        return Scheme.scheme(
+            name: name,
+            shared: true,
+            buildAction: .buildAction(targets: ["\(name)"]),
+            testAction: .targets(
+                ["\(name)Tests"],
+                configuration: config.configName,
+                options: .options(coverage: true, codeCoverageTargets: ["\(name)"])
+            ),
+            runAction: .runAction(configuration: config.configName),
+            archiveAction: .archiveAction(configuration: config.configName),
+            profileAction: .profileAction(configuration: config.configName),
+            analyzeAction: .analyzeAction(configuration: config.configName)
+        )
+    }
+
+    static func makeDemoScheme(name: String) -> Scheme {
+        return Scheme.scheme(
+            name: "\(name)Demo",
+            shared: true,
+            buildAction: .buildAction(targets: ["\(name)Demo"]),
+            runAction: .runAction(configuration: BuildConfig.dev.configName),
+            archiveAction: .archiveAction(configuration: BuildConfig.dev.configName),
+            profileAction: .profileAction(configuration: BuildConfig.dev.configName),
+            analyzeAction: .analyzeAction(configuration: BuildConfig.dev.configName)
+        )
+    }
+}
+
