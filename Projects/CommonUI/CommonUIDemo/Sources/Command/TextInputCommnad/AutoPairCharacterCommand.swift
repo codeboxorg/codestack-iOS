f26a2d8b08c7ab6e75d9ca01021c94d2ca9e1d5f
pHH <gudghks56@gmail.com>
2025-03-29 14:30:34 +0900

    feat: TextInput Command 추가

---
+++ Projects/CommonUI/CommonUIDemo/Sources/Command/TextInputCommnad/AutoPairCharacterCommand.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+struct AutoPairCharacterCommand: TextInputCommand {
+    
+    weak var editor: UITextView?
+    
+    init(_ editor: UITextView?) {
+        self.editor = editor
+    }
+    
+    func shouldHandle(text: String) -> Bool {
+        MatchingCharacter.matchingCharacter(text) != nil && text.first == text.last
+    }
+
+    func execute(range: NSRange, text: String) -> Bool {
+        guard let editor = editor else {
+            return true
+        }
+        
+        guard let value = MatchingCharacter.matchingCharacter(text) else {
+            return true
+        }
+        
+
+        let insertion = "\(value.rawValue)\(value.pair)"
+
+        if let start = editor.position(from: editor.beginningOfDocument, offset: range.location),
+           let end = editor.position(from: start, offset: range.length),
+           let replaceRange = editor.textRange(from: start, to: end) {
+            editor.replace(replaceRange, withText: insertion)
+        }
+
+        let cursorPosition = range.location + 1
+        
+        if let position = editor.position(from: editor.beginningOfDocument, offset: cursorPosition) {
+            editor.selectedTextRange = editor.textRange(from: position, to: position)
+        }
+        
+        return false
+    }
+}
