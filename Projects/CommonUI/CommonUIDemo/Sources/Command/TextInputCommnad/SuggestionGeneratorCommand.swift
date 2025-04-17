7d4fba1290bfb0a31f7139bcc6e2ab30c4f4977a
pHH <gudghks56@gmail.com>
2025-04-17 10:34:34 +0900

    feat: SuggestionGenerator TextInputCommand 추가
    - Suggestion Manager 에 의존
    - text Input을 받다가 문자가 아닌 입력을 받을 시 manager를 통해서 SuggestionWord 를 업데이트

---
+++ Projects/CommonUI/CommonUIDemo/Sources/Command/TextInputCommnad/SuggestionGeneratorCommand.swift
@@ -0,0 +1,39 @@
+import UIKit
+import Global
+
+struct SuggestionGeneratorCommand: TextInputCommand {
+    
+    weak var editor: UITextView?
+    weak var suggestionManager: SuggestionManager?
+    
+    init(
+        editor: UITextView?,
+        suggestionManager: SuggestionManager?
+    ) {
+        self.editor = editor
+        self.suggestionManager = suggestionManager
+    }
+    
+    func shouldHandle(text: String) -> Bool {
+        guard
+            text.count == 1,
+            let scalar = text.unicodeScalars.first
+        else {
+            return false
+        }
+        return CharacterSet.alphanumerics.inverted.contains(scalar)
+    }
+    
+    func execute(range: NSRange, text: String) -> Bool {
+        guard
+            let suggestionManager,
+            let word = suggestionManager.findPriorWord()
+        else {
+            return true
+        }
+        Log.debug("word: \(word)")
+        suggestionManager.input(for: word)
+        
+        return true
+    }
+}
