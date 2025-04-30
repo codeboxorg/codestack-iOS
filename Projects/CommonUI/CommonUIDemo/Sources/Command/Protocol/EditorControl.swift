4f43aaa5c0038aadef326146588e060ed1ff2e77
pHH <gudghks56@gmail.com>
2025-04-30 13:02:01 +0900

    refactor: move left right 함수

--- Projects/CommonUI/CommonUIDemo/Sources/Command/Protocol/EditorControl.swift
+++ Projects/CommonUI/CommonUIDemo/Sources/Command/Protocol/EditorControl.swift
@@ -3,7 +3,9 @@ import UIKit
 
 protocol EditorControl: AnyObject {
     var moveTimer: Timer? { get set }
-    var editor: UITextView? { get set }
+    var editor: UITextView? { get }
+    func moveLeftTimerTouchExecute()
+    func moveRightTimerTouchExecute()
 }
 
 
