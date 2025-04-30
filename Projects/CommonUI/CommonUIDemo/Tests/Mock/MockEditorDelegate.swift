538d4a164eb276b39d6a567378ae9dac95f029a4
pHH <gudghks56@gmail.com>
2025-04-30 13:03:25 +0900

    test: 의존성 refactor에 따른 Mock 파일 변경

--- Projects/CommonUI/CommonUIDemo/Tests/Mock/MockEditorDelegate.swift
+++ Projects/CommonUI/CommonUIDemo/Tests/Mock/MockEditorDelegate.swift
@@ -1,17 +1,46 @@
 @testable import CommonUIDemo
 import UIKit
+import Global
 
-
-class MockEditorDelegate: NSObject, UITextViewDelegate {
-    var manager: TextInputCommandExcuteManager!
+final class MockEditorDelegate: NSObject, UITextViewDelegate {
+    
+    var textInputCommandExcuteManager: TextInputCommandExcuteManager!
+    var suggestionManager: SuggestionManager!
+    var cursorCommand: SuggestFocusCusorCommand
+    
+    init(
+        textInputCommandExcuteManager: TextInputCommandExcuteManager!,
+        suggestionManager: SuggestionManager!,
+        cusorCommand: SuggestFocusCusorCommand
+    ) {
+        self.textInputCommandExcuteManager = textInputCommandExcuteManager
+        self.suggestionManager = suggestionManager
+        self.cursorCommand = cusorCommand
+    }
+    
+    func textViewDidChangeSelection(_ textView: UITextView) {
+        Log.debug("textViewDidChangeSelection")
+        if cursorCommand.shouldHandle {
+            cursorCommand.execute()
+        }
+    }
     
     func textViewDidChange(_ textView: UITextView) {
-        if let (oldTextRange, nsRange, text) = manager.systemInsertUpdate {
-            manager.systemInsertActionSnapShot(
-                oldTextRange: oldTextRange,
-                shouldChangeTextIn: nsRange,
-                replacementText: text
-            )
+        Log.debug("textViewDidChange")
+        suggestionManager.suggestionLayoutGenerate()
+        if let (oldTextRange, range, text) = SystemInsertSnapShot.shared.useWhenTextDidChange {
+            textInputCommandExcuteManager
+                .systemInsertActionSnapShot(
+                    oldTextRange: oldTextRange,
+                    shouldChangeTextIn: range,
+                    replacementText: text
+                )
+            SystemInsertSnapShot.shared.useWhenTextDidChange = nil
         }
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        Log.debug("shouldChangeTextIn")
+        return textInputCommandExcuteManager.commandExecute(shouldChangeTextIn: range, replacementText: text)
     }
 }
---
+++ Projects/CommonUI/CommonUIDemo/Tests/Mock/MockWidthTracker.swift
@@ -0,0 +1,13 @@
+
+@testable import CommonUIDemo
+import UIKit
+
+class MockTextViewWidthUpdate: TextViewWidthUpdateProtocol {
+    func updateTextViewWidth(_ width: CGFloat) {
+        
+    }
+    
+    func positioningScrollView() {
+        
+    }
+}
