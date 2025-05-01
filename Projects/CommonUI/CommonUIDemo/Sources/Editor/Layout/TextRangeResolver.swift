6433d0228eab0d52e0f57108a01ad8fc184980a0
pHH <gudghks56@gmail.com>
2025-05-01 12:39:07 +0900

    refactor: UITextView Extensiion 파일로 이동 하여 삭제

--- Projects/CommonUI/CommonUIDemo/Sources/Editor/Layout/TextRangeResolver.swift
+++ Projects/CommonUI/CommonUIDemo/Sources/Editor/Layout/TextRangeResolver.swift
@@ -1,28 +1,5 @@
 import UIKit
 
-
-extension UITextView {
-    func textRange(from beginning: UITextPosition, offset: Int, length: Int) -> UITextRange? {
-        guard
-            let start = self.position(from: beginning, offset: offset),
-            let end = self.position(from: start, offset: length) else {
-            return nil
-        }
-        
-        return self.textRange(from: start, to: end)
-    }
-    
-    func textRange(from nsRange: NSRange) -> UITextRange? {
-        guard
-            let start = self.position(from: self.beginningOfDocument, offset: nsRange.location),
-            let end = self.position(from: start, offset: nsRange.length)
-        else {
-            return nil
-        }
-        return self.textRange(from: start, to: end)
-    }
-}
-
 struct TextRangeResolver {
     weak var editor: UITextView!
     
