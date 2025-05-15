814bf3d259cb82c5784781a9d77fabc8d809c3f8
pHH <gudghks56@gmail.com>
2025-05-15 16:44:22 +0900

    remove: 파일로 분리

--- Projects/CommonUI/CommonUIDemo/Sources/Editor/Manager/SuggestionManager.swift
+++ Projects/CommonUI/CommonUIDemo/Sources/Editor/Manager/SuggestionManager.swift
@@ -1,50 +1,6 @@
 import UIKit
 import Global
 
-protocol SuggestionManager: AnyObject {
-    func initSuggestion(string: [String])
-    func remove(for text: String)
-    var currentFocusingItem: String { get set }
-}
-
-extension SuggestionManager {
-    func initSuggestion(string: [String] = []) {
-        self.initSuggestion(string: string)
-    }
-}
-
-protocol SuggestionViewRemoval: AnyObject {
-    func removeSuggestionView()
-}
-
-protocol SuggestionLayoutGenerator: AnyObject {
-    func suggestionLayoutGenerate()
-}
-
-protocol SuggestionCusorPosition: AnyObject {
-    func excuteWhenCusorPositionChanged()
-    var isSuggestionFocusingState: Bool { get }
-}
-
-protocol SuggestionInsert: AnyObject {
-    func insertCurrentFocusingSuggestionWord()
-}
-
-protocol SuggestionGenerator: AnyObject {
-    func input(for text: String)
-    func findPriorWord() -> String?
-}
-
-
-typealias CompositeSuggestionManager = (
-    SuggestionManager &
-    SuggestionViewRemoval &
-    SuggestionLayoutGenerator &
-    SuggestionCusorPosition &
-    SuggestionInsert &
-    SuggestionGenerator
-)
-
 protocol SuggestionCommandProtocol {
     func findPriorWord() -> String?
     func findPriorCusorWords() -> (word: String, position: UITextPosition)?
