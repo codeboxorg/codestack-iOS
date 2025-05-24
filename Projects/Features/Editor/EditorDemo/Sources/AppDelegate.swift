a48e5ff4be7b9cafdb075843b006a2ee34b0e8d1
pHH <gudghks56@gmail.com>
2025-05-24 18:44:59 +0900

    move: CommonUI(Editor) -> Editor Module

---
+++ Projects/Features/Editor/EditorDemo/Sources/AppDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  AppDelegate.swift
+//  Editor
+//
+//  Created by hwan on 5/24/25.
+//  Copyright © 2025 com.hwan. All rights reserved.
+//
+
+import Foundation
---
+++ Projects/Features/Editor/EditorDemo/Sources/SceneDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  SceneDelegate.swift
+//  Editor
+//
+//  Created by hwan on 5/24/25.
+//  Copyright © 2025 com.hwan. All rights reserved.
+//
+
+import Foundation
